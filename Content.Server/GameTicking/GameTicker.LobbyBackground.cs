b8e13b897a868a8d33fd0b22572dd30dbabfdf4f
Matthew Herber <matthewherber@sbcglobal.net>
2025-10-16 13:58:47 -0400

    initial working prototype

--- Content.Server/GameTicking/GameTicker.LobbyBackground.cs
+++ Content.Server/GameTicking/GameTicker.LobbyBackground.cs
@@ -1,4 +1,5 @@
-using Content.Server.GameTicking.Prototypes;
+using Content.Shared.GameTicking.Prototypes;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
 using System.Linq;
@@ -8,22 +9,34 @@ namespace Content.Server.GameTicking;
 public sealed partial class GameTicker
 {
     [ViewVariables]
-    public string? LobbyBackground { get; private set; }
+    public ProtoId<LobbyBackgroundPrototype>? LobbyBackground { get; set; }
 
     [ViewVariables]
-    private List<ResPath>? _lobbyBackgrounds;
+    private List<ProtoId<LobbyBackgroundPrototype>>? _lobbyBackgrounds;
 
     // STARLIGHT: Support for conditional lobby backgrounds
-    private string? _forcedLobbyBackground;
+    private ProtoId<LobbyBackgroundPrototype>? _forcedLobbyBackground;
 
     private static readonly string[] WhitelistedBackgroundExtensions = new string[] {"png", "jpg", "jpeg", "webp"};
 
     private void InitializeLobbyBackground()
     {
-        _lobbyBackgrounds = _prototypeManager.EnumeratePrototypes<LobbyBackgroundPrototype>()
-            .Select(x => x.Background)
-            .Where(x => WhitelistedBackgroundExtensions.Contains(x.Extension))
-            .ToList();
+        var allprotos = _prototypeManager.EnumeratePrototypes<LobbyBackgroundPrototype>().ToList();
+        //create protoids from them
+        foreach (var proto in allprotos)
+        {
+            var ext = proto.Background.Extension;
+            if (WhitelistedBackgroundExtensions.Contains(ext))
+            {
+                //create a protoid and add it to the list
+                _lobbyBackgrounds ??= new List<ProtoId<LobbyBackgroundPrototype>>();
+                _lobbyBackgrounds.Add(new ProtoId<LobbyBackgroundPrototype>(proto.ID));
+            }
+            else
+            {
+                Logger.Warning($"Lobby background {proto.ID} has an invalid extension {ext}. Must be one of: {string.Join(", ", WhitelistedBackgroundExtensions)}");
+            }
+        }
 
         RandomizeLobbyBackground();
     }
@@ -36,16 +49,26 @@ public sealed partial class GameTicker
             _forcedLobbyBackground = null; // Reset after use
             return;
         }
-        
-        LobbyBackground = _lobbyBackgrounds!.Any() ? _robustRandom.Pick(_lobbyBackgrounds!).ToString() : null;
+
+        if (_lobbyBackgrounds!.Any())
+        {
+            Logger.Info($"Choosing from {_lobbyBackgrounds!.Count} valid lobby backgrounds.");
+            LobbyBackground = _robustRandom.Pick(_lobbyBackgrounds!);
+            Logger.Info($"Chosen lobby background: {LobbyBackground}");
+        }
+        else
+        {
+            Logger.Error("No valid lobby backgrounds found. Make sure at least one is defined in the prototypes and has a valid image extension (png, jpg, jpeg, webp).");
+            LobbyBackground = null;
+        }
     }
 
     /// <summary>
     /// STARLIGHT: Sets a specific lobby background to be used on the next round restart.
     /// </summary>
-    /// <param name="backgroundPath">The path to the background image</param>
-    public void SetLobbyBackground(string backgroundPath)
+    /// <param name="lobbyProto">The path to the background image</param>
+    public void SetLobbyBackground(ProtoId<LobbyBackgroundPrototype> lobbyProto)
     {
-        _forcedLobbyBackground = backgroundPath;
+        _forcedLobbyBackground = lobbyProto;
     }
 }
