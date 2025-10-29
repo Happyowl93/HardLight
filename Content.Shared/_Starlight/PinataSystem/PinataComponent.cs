74cf4804d228c17c1f4d1d95a7dfbe92c11dfeea
Conflee <46701008+Conflee@users.noreply.github.com>
2025-10-29 10:18:32 -0400

    Update PinataComponent.cs

--- Content.Shared/_Starlight/PinataSystem/PinataComponent.cs
+++ Content.Shared/_Starlight/PinataSystem/PinataComponent.cs
@@ -1,4 +1,7 @@
-﻿using Content.Shared.EntityTable.EntitySelectors;
+//Copyright © 2025 .cerol (Discord), Licensed under MIT License.
+//Changes after https://github.com/ss14Starlight/space-station-14/pull/2054/commits/e18dafedad110b20cdc17d054fe35413a1831f59 licensed under Starlight License.
+
+using Content.Shared.EntityTable.EntitySelectors;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._Starlight.PinataSystem;
