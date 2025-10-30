552ad2c8025ce0b4a5150e92c88b28910bdcde65
Fasuh <gac@welk9ZILT2chee>
2025-10-30 20:56:29 +0100

    fix(Uppies): Fix uppies module being not escapable from.

--- Content.Server/Resist/EscapeInventorySystem.cs
+++ Content.Server/Resist/EscapeInventorySystem.cs
@@ -8,6 +8,8 @@ using Content.Shared.Inventory;
 using Content.Shared.Movement.Events;
 using Content.Shared.Resist;
 using Content.Shared.Storage;
+using Content.Shared.Tag; // Starlight Edit
+using Robust.Server.GameObjects; // Starlight Edit
 using Robust.Shared.Containers;
 
 namespace Content.Server.Resist;
@@ -19,6 +21,8 @@ public sealed class EscapeInventorySystem : EntitySystem
     [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
     [Dependency] private readonly ActionBlockerSystem _actionBlockerSystem = default!;
     [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
+    [Dependency] private readonly TagSystem _tagSystem = default!; // Starlight Edit
+    [Dependency] private readonly TransformSystem _transformSystem = default!; // Starlight Edit
 
     public override void Initialize()
     {
@@ -53,7 +57,18 @@ public sealed class EscapeInventorySystem : EntitySystem
 
         // Uncontested
         if (HasComp<StorageComponent>(container.Owner) || HasComp<InventoryComponent>(container.Owner) || HasComp<SecretStashComponent>(container.Owner))
+        // Starlight edit start - Add another escapable container
+        {
             AttemptEscape(uid, container.Owner, component);
+            return;
+        }
+        
+        // Uncontested - Escape from borg modules and such
+        if (_tagSystem.HasTag(container.Owner, "PersonnelStorage"))
+        {
+            AttemptEscape(uid, container.Owner, component);
+        }
+        // Starlight edit end
     }
 
     private void AttemptEscape(EntityUid user, EntityUid container, CanEscapeInventoryComponent component, float multiplier = 1f)
@@ -82,9 +97,53 @@ public sealed class EscapeInventorySystem : EntitySystem
         if (args.Handled || args.Cancelled)
             return;
 
-        _containerSystem.AttachParentToContainerOrGrid((uid, Transform(uid)));
+        // Starlight edit start - Special handling for borg modules
+        if (_containerSystem.TryGetContainingContainer((uid, null, null), out var container) &&
+            _tagSystem.HasTag(container.Owner, "PersonnelStorage"))
+        {
+            EscapeFromPersonnelStorage(uid);
+        }
+        else
+        {
+            _containerSystem.AttachParentToContainerOrGrid((uid, Transform(uid)));
+        }
+        // Starlight edit end
         args.Handled = true;
     }
+    // Starlight edit start - Special handling for borg modules
+    /// <summary>
+    /// Special handling for borg modules, it is required because in some cases borgs and their modules are two separate containers,
+    /// we recursively remove them from those containers until we hit a container that is not an escape source.
+    /// This is kinda needed to handle a case where a borg would be inside of a locker and someone tried to escape for it, to make it so they stay inside of the locker.
+    /// </summary>
+    private void EscapeFromPersonnelStorage(EntityUid uid)
+    {
+        while (true)
+        {
+            if (!_containerSystem.TryGetContainingContainer((uid, null, null), out var container))
+            {
+                var transform = Transform(uid);
+                _transformSystem.AttachToGridOrMap(uid, transform);
+                return;
+            }
+            
+            bool isEscapeTarget = _handsSystem.IsHolding(container.Owner, uid, out _) ||
+                                  HasComp<StorageComponent>(container.Owner) ||
+                                  HasComp<InventoryComponent>(container.Owner) ||
+                                  HasComp<SecretStashComponent>(container.Owner) ||
+                                  _tagSystem.HasTag(container.Owner, "PersonnelStorage");
+            
+            if (isEscapeTarget)
+            {
+                _containerSystem.Remove(uid, container, force: true);
+                continue;
+            }
+            
+            _containerSystem.Insert(uid, container);
+            return;
+        }
+    }
+    // Starlight edit end
 
     private void OnDropped(EntityUid uid, CanEscapeInventoryComponent component, DroppedEvent args)
     {
