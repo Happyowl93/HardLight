c9716c2c05a7f8962922dfa2b4e46df09b4df9c4
CrazyPhantom779 <izayussedwards@gmail.com>
2025-11-13 16:14:08 +1100

    Reinforced Secret door

--- Content.Client/IconSmoothing/IconSmoothSystem.cs
+++ Content.Client/IconSmoothing/IconSmoothSystem.cs
@@ -60,6 +60,15 @@ namespace Content.Client.IconSmoothing
                 return;
 
             SetCornerLayers((uid, sprite), component);
+            // Starlight Start: Conditional Smoothing
+            if (!xform.Anchored)
+            {
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.NE, false);
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.SE, false);
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.SW, false);
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.NW, false);
+            }
+            // Starlight End
 
             if (component.Shader != null)
             {
@@ -205,6 +214,15 @@ namespace Content.Client.IconSmoothing
         {
             if (!args.Detaching)
                 _anchorChangedEntities.Enqueue(uid);
+            // Starlight Start: Conditional Smoothing
+            if (!args.Detaching && component.Mode == IconSmoothingMode.Corners && TryComp(uid, out SpriteComponent? sprite))
+            {
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.NE, true);
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.SE, true);
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.SW, true);
+                _sprite.LayerSetVisible((uid, sprite), CornerLayers.NW, true);
+            }
+            // Starlight End
         }
 
         private void CalculateNewSprite(EntityUid uid,
@@ -270,7 +288,7 @@ namespace Content.Client.IconSmoothing
             // Check entity for disabled states
             if (smooth.DisabledStates != null && smooth.DisabledStates.Count > 0 && !smooth.ShouldSmooth())
             {
-                // Hide corner overlays when smoothing is disabled
+                // Hide corner corners when smoothing is disabled
                 if (smooth.Mode == IconSmoothingMode.Corners)
                 {
                     _sprite.LayerSetVisible(spriteEnt, CornerLayers.NE, false);
@@ -280,8 +298,6 @@ namespace Content.Client.IconSmoothing
                 }
                 return;
             }
-
-            // If previously hidden due to the above, enable
             if (smooth.Mode == IconSmoothingMode.Corners && smooth.DisabledStates != null && smooth.DisabledStates.Count > 0 && smooth.ShouldSmooth())
             {
                 _sprite.LayerSetVisible(spriteEnt, CornerLayers.NE, true);
@@ -303,6 +319,19 @@ namespace Content.Client.IconSmoothing
                     return;
                 }
             }
+            // Starlight Start: Conditional Smoothing
+            else
+            {
+                if (smooth.Mode == IconSmoothingMode.Corners)
+                {
+                    _sprite.LayerSetVisible(spriteEnt, CornerLayers.NE, false);
+                    _sprite.LayerSetVisible(spriteEnt, CornerLayers.SE, false);
+                    _sprite.LayerSetVisible(spriteEnt, CornerLayers.SW, false);
+                    _sprite.LayerSetVisible(spriteEnt, CornerLayers.NW, false);
+                }
+                return;
+            }
+            // Starlight End
 
             switch (smooth.Mode)
             {
