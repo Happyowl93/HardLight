0fba34cf80517f1076e93c199e469504bbe70122
Penguinwizzard <penguinwizzard@github.com>
2025-11-17 18:18:16 -0800

    Make damaged plating repairable
    This allows using a welder to convert PlatingDamaged to Plating.

--- Content.Shared/Maps/TileSystem.cs
+++ Content.Shared/Maps/TileSystem.cs
@@ -153,8 +153,11 @@ public sealed class TileSystem : EntitySystem
                 (_robustRandom.NextFloat() - 0.5f) * bounds));
 
         //Actually spawn the relevant tile item at the right position and give it some random offset.
-        var tileItem = Spawn(tileDef.ItemDropPrototypeName, coordinates);
-        Transform(tileItem).LocalRotation = _robustRandom.NextDouble() * Math.Tau;
+        if (!string.IsNullOrWhiteSpace(tileDef.ItemDropPrototypeName)) // Starlight - condition tile item drop on it being a non-empty prototype name
+        { // Starlight - add enclosing block
+            var tileItem = Spawn(tileDef.ItemDropPrototypeName, coordinates);
+            Transform(tileItem).LocalRotation = _robustRandom.NextDouble() * Math.Tau;
+        } // Starlight - add enclosing block
 
         // Destroy any decals on the tile
         var decals = _decal.GetDecalsInRange(gridUid, coordinates.SnapToGrid(EntityManager, _mapManager).Position, 0.5f);
