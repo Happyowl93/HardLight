2c6a431f4e3799923200cc883d2bda43aa3652d1
FoxxoTrystan <trystan.garnierhein@gmail.com>
2025-11-30 00:56:23 +0100

    Test fixes?

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -42,6 +42,7 @@ namespace Content.IntegrationTests.Tests
                     .Where(p => !pair.IsTestPrototype(p))
                     .Where(p => !p.Components.ContainsKey("MapGrid")) // This will smash stuff otherwise.
                     .Where(p => !p.Components.ContainsKey("RoomFill")) // This comp can delete all entities, and spawn others
+                    .Where(p => !p.Components.ContainsKey("Explosion")) // Starlight - This might destroy others ents and cause issues.
                     .Select(p => p.ID)
                     .ToList();
 
@@ -105,6 +106,7 @@ namespace Content.IntegrationTests.Tests
                     .Where(p => !pair.IsTestPrototype(p))
                     .Where(p => !p.Components.ContainsKey("MapGrid")) // This will smash stuff otherwise.
                     .Where(p => !p.Components.ContainsKey("RoomFill")) // This comp can delete all entities, and spawn others
+                    .Where(p => !p.Components.ContainsKey("Explosion")) // Starlight - This might destroy others ents and cause issues.
                     .Select(p => p.ID)
                     .ToList();
                 foreach (var protoId in protoIds)
