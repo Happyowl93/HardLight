d3400da588454081313f18409bf5fb22633d8969
Rinary <rinary.super@gmail.com>
2025-12-01 17:44:11 +0200

    init

--- Content.Shared/_Starlight/MassDriver/EntitySystems/SharedMassDriverSystem.cs
+++ Content.Shared/_Starlight/MassDriver/EntitySystems/SharedMassDriverSystem.cs
@@ -4,6 +4,7 @@ using Content.Shared.Power;
 using Content.Shared.Power.EntitySystems;
 using Content.Shared.Audio;
 using Robust.Shared.Timing;
+using Robust.Shared.GameObjects;
 
 namespace Content.Shared._Starlight.MassDriver.EntitySystems;
 
@@ -62,7 +63,7 @@ public abstract class SharedMassDriverSystem : EntitySystem
             Dirty(uid, massDriver);
 
             entities.Clear();
-            _lookup.GetEntitiesIntersecting(uid, entities);
+            _lookup.GetEntitiesIntersecting(uid, entities, LookupFlags.Dynamic | LookupFlags.Approximate);
 
             // Remove anchored entities since we can't throw them
             entities.RemoveWhere(e => Transform(e).Anchored);
