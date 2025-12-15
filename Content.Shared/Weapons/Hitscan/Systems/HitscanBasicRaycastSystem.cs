a31826f43e3c7cde3ab64f214281b28e7256f6a6
FoxxoTrystan <trystan.garnierhein@gmail.com>
2025-12-15 23:49:51 +0100

    fixhitscan

--- Content.Shared/Weapons/Hitscan/Systems/HitscanBasicRaycastSystem.cs
+++ Content.Shared/Weapons/Hitscan/Systems/HitscanBasicRaycastSystem.cs
@@ -67,11 +67,12 @@ public sealed class HitscanBasicRaycastSystem : EntitySystem
         // Otherwise:
         //  1.) Hit the first entity that you targeted.
         //  2.) Hit the first entity that doesn't require you to aim at it specifically to be hit.
+        // Ignore raycast results that hit a NullSpace entity. // TODO: Do something better?
         var result = _container.IsEntityOrParentInContainer(shooter)
             ? rayCastResults.FirstOrNull()
-            : rayCastResults.FirstOrNull(hit => hit.HitEntity == target
-                                                || CompOrNull<RequireProjectileTargetComponent>(hit.HitEntity)?.Active != true
-                                                || HasComp<NullSpaceComponent>(hit.HitEntity));
+            : rayCastResults.FirstOrNull(hit => (hit.HitEntity == target
+                                                || CompOrNull<RequireProjectileTargetComponent>(hit.HitEntity)?.Active != true)
+                                               && CompOrNull<NullSpaceComponent>(hit.HitEntity) == null);
 
         var distanceTried = result?.Distance ?? ent.Comp.MaxDistance;
 
