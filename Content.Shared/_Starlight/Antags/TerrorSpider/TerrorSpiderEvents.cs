8dbaf11341cc70a1d18ad4b5bd135d06042f270a
Rinary <rinary.super@gmail.com>
2025-12-18 16:21:17 +0200

    acid vent action

--- Content.Shared/_Starlight/Antags/TerrorSpider/TerrorSpiderEvents.cs
+++ Content.Shared/_Starlight/Antags/TerrorSpider/TerrorSpiderEvents.cs
@@ -1,3 +1,7 @@
+﻿using Content.Shared.Actions;
+
 namespace Content.Shared._Starlight.Antags.TerrorSpider;
 
 public sealed class EggsInjectedEvent : EntityEventArgs;
+
+public sealed partial class AcidVentEvent : EntityTargetActionEvent;
