acbb9925aea58f6c0fe935166560185a7a137677
Jackson Huffstutler <jack.huffstutler@gmail.com>
2025-12-30 22:01:08 -0600

    Changed snake case to camel case for consistency

--- Content.Shared/Xenobiology/SlimeComponent.cs
+++ Content.Shared/Xenobiology/SlimeComponent.cs
@@ -21,25 +21,25 @@ public sealed partial class SlimeComponent : Component
     /// <summary>
     /// The amount the nutrition changes every second. Positive means the nutrition will increase, negative means the nutrition will decrease. Cannot go below 0.
     /// </summary>
-    [DataField("nutrition_change_per_second", required: true)]
+    [DataField("nutritionChangePerSecond", required: true)]
     public FixedPoint2 NutritionChangePerSecond;
     
     /// <summary>
     /// The amount of damage dealt to the target entity wheb the slime eats.
     /// </summary>
-    [DataField("damage_on_eat", required: true)]
+    [DataField("damageOnEat", required: true)]
     public DamageSpecifier DamageOnEat;
     
     /// <summary>
     /// The amount of nutrition the slime gains on each eat.
     /// </summary>
-    [DataField("nutrition_on_hit", required: true)]
+    [DataField("nutritionOnHit", required: true)]
     public FixedPoint2 NutritionOnHit;
     
     /// <summary>
     /// What this slime splits into if not mutating
     /// </summary>
-    [DataField("split_into", required: true)]
+    [DataField("splitInto", required: true)]
     public string SplitInto;
 }
 
