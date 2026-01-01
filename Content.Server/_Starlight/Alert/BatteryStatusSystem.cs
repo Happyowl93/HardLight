5b1886dafedd986c4eef910fab17231ef98202cd
Rinary <rinary.super@gmail.com>
2026-01-01 03:04:47 +0200

    Apply suggestions from code review

--- Content.Server/_Starlight/Alert/BatteryStatusSystem.cs
+++ Content.Server/_Starlight/Alert/BatteryStatusSystem.cs
@@ -1,36 +1,49 @@
 using Content.Server.PowerCell;
 using Content.Shared._Starlight.UI;
 using Content.Shared.Alert;
+using Content.Shared.PowerCell;
 using Content.Shared.PowerCell.Components;
 
 namespace Content.Server._Starlight.Alert;
 
-public sealed partial class BatteryStatusSystem : EntitySystem
+public sealed partial class BatteryAlertSystem : EntitySystem
 {
     [Dependency] private readonly AlertsSystem _alerts = default!;
     [Dependency] private readonly PowerCellSystem _powerCell = default!;
-    public void UpdateBatteryAlert(EntityUid uid, PowerCellSlotComponent? slotComponent = null)
+    
+    public override void Initialize()
     {
-        if (TryComp<BatteryStatusComponent>(uid, out var comp))
+        SubscribeLocalEvent<BatteryAlertComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<BatteryAlertComponent, PowerCellChangedEvent>(OnPowerCellChanged);
+    }
+    
+    private void OnMapInit(EntityUid uid, BatteryAlertComponent component, MapInitEvent args) => TryUpdateBatteryAlert(uid, component);
+    
+    private void OnPowerCellChanged(EntityUid uid, BatteryAlertComponent component, PowerCellChangedEvent args) => TryUpdateBatteryAlert(uid, component);
+    
+    public bool TryUpdateBatteryAlert(EntityUid uid, BatteryAlertComponent? = null comp, PowerCellSlotComponent? slotComponent = null)
+    {
+        if (!Resolve(uid, ref comp))
+            return false;
+        
+        if (!_powerCell.TryGetBatteryFromSlot(uid, out var battery, slotComponent))
         {
-            if (!_powerCell.TryGetBatteryFromSlot(uid, out var battery, slotComponent))
-            {
-                _alerts.ClearAlert(uid, comp.BatteryAlert);
-                _alerts.ShowAlert(uid, comp.NoBatteryAlert);
-                return;
-            }
-
-            var chargePercent = (short)MathF.Round(battery.CurrentCharge / battery.MaxCharge * 10f);
-
-            // we make sure 0 only shows if they have absolutely no battery.
-            // also account for floating point imprecision
-            if (chargePercent == 0 && _powerCell.HasDrawCharge(uid, cell: slotComponent))
-            {
-                chargePercent = 1;
-            }
-
-            _alerts.ClearAlert(uid, comp.NoBatteryAlert);
-            _alerts.ShowAlert(uid, comp.BatteryAlert, chargePercent);
+            _alerts.ClearAlert(uid, comp.BatteryAlert);
+            _alerts.ShowAlert(uid, comp.NoBatteryAlert);
+            return true;
         }
+
+        var chargePercent = (short)MathF.Round(battery.CurrentCharge / battery.MaxCharge * 10f);
+
+        // we make sure 0 only shows if they have absolutely no battery.
+        // also account for floating point imprecision
+        if (chargePercent == 0 && _powerCell.HasDrawCharge(uid, cell: slotComponent))
+        {
+            chargePercent = 1;
+        }
+
+        _alerts.ClearAlert(uid, comp.NoBatteryAlert);
+        _alerts.ShowAlert(uid, comp.BatteryAlert, chargePercent);
+        return true;
     }
 }
--- Content.Shared/_Starlight/UI/BatteryStatusComponent.cs
+++ Content.Shared/_Starlight/UI/BatteryStatusComponent.cs
@@ -4,7 +4,7 @@ using Robust.Shared.Prototypes;
 namespace Content.Shared._Starlight.UI;
 
 [RegisterComponent]
-public sealed partial class BatteryStatusComponent : Component
+public sealed partial class BatteryAlertComponent : Component
 {
     [DataField]
     public ProtoId<AlertPrototype> BatteryAlert = "BorgBattery";
