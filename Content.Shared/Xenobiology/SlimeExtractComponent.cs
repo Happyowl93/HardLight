8b66c43f67f6cb4eb7dea0e5347600010e2eecec
Jackson Huffstutler <jack.huffstutler@gmail.com>
2025-12-31 19:15:26 -0600

    Added minor docs

--- Content.Shared/Xenobiology/SlimeExtractComponent.cs
+++ Content.Shared/Xenobiology/SlimeExtractComponent.cs
@@ -15,16 +15,29 @@ public sealed partial class SlimeExtractComponent : Component
     [DataField("extractReactions", required: true)]
     public List<ExtractReaction> ExtractReactions = new();
     
+    /// <summary>
+    /// The name of the container that holds the solution.
+    /// Needed so that the slime extract can communicate with the container itself.
+    /// </summary>
     [DataField("containerName", required: true)]
     public string ContainerName = string.Empty;
 }
 
+/// <summary>
+/// A set of requirements and the associated effects.
+/// </summary>
 [Serializable, NetSerializable, DataDefinition]
 public sealed partial class ExtractReaction
 {
+    /// <summary>
+    /// The minimum reagent requirements.
+    /// </summary>
     [DataField("requirements", required: true)]
     public Solution Requirements = default!;
 
+    /// <summary>
+    /// The effects caused when there is enough of the required reagents.
+    /// </summary>
     [DataField("effects", required: true)]
     public List<EntityEffect> Effects = default!;
 }
