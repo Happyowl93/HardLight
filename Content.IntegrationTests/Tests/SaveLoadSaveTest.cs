6a6bf68a1ef02265b84455175a1ea96f508b5a3d
Matthew Herber <matthew.herber@workwithgrid.com>
2026-01-05 10:36:44 -0500

    Get all tests passing

--- Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
+++ Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
@@ -88,7 +88,7 @@ namespace Content.IntegrationTests.Tests
             await pair.CleanReturnAsync();
         }
 
-        private const string TestMap = "Maps/_Starlight/Stations/Cog.yml";
+        private const string TestMap = "Maps/_Starlight/Stations/Saltern.yml";
 
         /// <summary>
         ///     Loads the default map, runs it for 5 ticks, then assert that it did not change.
