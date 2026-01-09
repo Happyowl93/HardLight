f55d3611cea421c743c592e33eaf6272880fa97f
Jackson Huffstutler <jack.huffstutler@gmail.com>
2026-01-09 10:53:16 -0600

    Added xenobiology console and camera tagging for specific cameras.

---
+++ Content.Shared/_Starlight/Xenobiology/XenobiologyConsoleComponent.cs
@@ -0,0 +1,9 @@
+using Content.Shared.Pinpointer;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Starlight.Xenobiology;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class XenobiologyConsoleComponent : Component
+{
+}
