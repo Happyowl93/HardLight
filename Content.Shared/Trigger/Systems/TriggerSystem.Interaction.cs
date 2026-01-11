a3a2ea38fc9ad99b8a1f550ea25cd988deb9e0ef
KarmaKitsuna <130065372+KarmaKitsuna@users.noreply.github.com>
2026-01-11 16:19:25 -0500

    Added C# Code

--- Content.Shared/Trigger/Systems/TriggerSystem.Interaction.cs
+++ Content.Shared/Trigger/Systems/TriggerSystem.Interaction.cs
@@ -24,6 +24,8 @@ public sealed partial class TriggerSystem
         SubscribeLocalEvent<ItemToggleOnTriggerComponent, TriggerEvent>(HandleItemToggleOnTrigger);
         SubscribeLocalEvent<AnchorOnTriggerComponent, TriggerEvent>(HandleAnchorOnTrigger);
         SubscribeLocalEvent<UseDelayOnTriggerComponent, TriggerEvent>(HandleUseDelayOnTrigger);
+        
+        SubscribeLocalEvent<TriggerOnUiOpenComponent, BoundUIOpenedEvent>(OnUiOpened); 
     }
 
     private void OnExamined(Entity<TriggerOnExaminedComponent> ent, ref ExaminedEvent args)
@@ -136,5 +138,13 @@ public sealed partial class TriggerSystem
             return;
 
         args.Handled |= _useDelay.TryResetDelay(target.Value, ent.Comp.CheckDelayed);
+    }
+
+    private void OnUiOpened(Entity<TriggerOnUiOpenComponent> ent, ref BoundUIOpenedEvent args) // Starlight Edit
+    {
+        if (ent.Comp.UiKeys == null || ent.Comp.UiKeys.Contains(args.UiKey))
+        {
+            Trigger(ent, args.Actor, ent.Comp.KeyOut);
+        }
     }
 }
