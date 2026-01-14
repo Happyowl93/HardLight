6ffa1538417f7dcc558d34760a39390ffd02fffd
furrylover833 <82061820+furrylover833@users.noreply.github.com>
2026-01-14 18:56:27 +0100

    patch

--- Content.Server/Mech/Systems/MechSystem.cs
+++ Content.Server/Mech/Systems/MechSystem.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Content.Server.Atmos.Components;
 using Content.Server.Atmos.EntitySystems;
 using Content.Server.Atmos.Piping.Components;
+using Content.Server.Body.Systems;
 using Content.Server.Hands.Systems;
 using Content.Server.Mech.Components;
 using Content.Server.Power.EntitySystems;
@@ -66,7 +67,7 @@ public sealed partial class MechSystem : SharedMechSystem
     [Dependency] private readonly NpcFactionSystem _factionSystem = default!;
     [Dependency] private readonly SharedPointLightSystem _light = default!;
     [Dependency] private readonly PowerCellSystem _powerCell = default!;
-    [Dependency] protected readonly IGameTiming Timing = default!;
+    [Dependency] private readonly IGameTiming Timing = default!;
     [Dependency] private readonly GasTankSystem _gasTank = default!;
 
 
@@ -573,7 +574,7 @@ public sealed partial class MechSystem : SharedMechSystem
         var state = new MechBoundUiState
         {
             EquipmentStates = ev.States,
-            Equipment = component.EquipmentContainer.ContainedEntities.Select(o => _entityManager.GetNetEntity(o)).ToList() // Starlight-edit: Correct equipment update
+            Equipment = component.EquipmentContainer.ContainedEntities.Select(o => GetNetEntity(o)).ToList() // Starlight-edit: Correct equipment update
         };
         _ui.SetUiState(uid, MechUiKey.Key, state);
     }
