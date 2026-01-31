e4868e1f0586c1140d71d75ed7f696dff460c116
Lily Autumn <realprismaticyt@gmail.com>
2026-02-01 07:42:08 +1300

    minor autogamma tweaks

--- Content.Server/Revolutionary/RevSupplyRiftSystem.cs
+++ Content.Server/Revolutionary/RevSupplyRiftSystem.cs
@@ -29,6 +29,7 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 using System.Linq;
 using Content.Server.AlertLevel; // starlight
+using Content.Server.RoundEnd; // starlight
 using Content.Server.Station.Systems;
 using Content.Shared.Starlight.CCVar; // starlight
 using Robust.Shared.Audio;
@@ -57,6 +58,7 @@ public sealed class RevSupplyRiftSystem : EntitySystem
     [Dependency] private readonly AlertLevelSystem _alert = default!; // Starlight
     [Dependency] private readonly StationSystem _station = default!; // starlight
     [Dependency] private readonly IConfigurationManager _config = default!; // Starlight
+    [Dependency] private readonly RoundEndSystem _roundEnd = default!; // starlight
 
     private const string RevSupplyRiftListingId = "RevSupplyRiftListing";
     
@@ -496,7 +498,7 @@ public sealed class RevSupplyRiftSystem : EntitySystem
             
         }
 
-        if (activeRiftCount >= _config.GetCVar(StarlightCCVars.AutogammaRiftCount) && _config.GetCVar(StarlightCCVars.AutogammaRiftEnabled)) //#region Starlight Autogamma
+        if (activeRiftCount == _config.GetCVar(StarlightCCVars.AutogammaRiftCount) && _config.GetCVar(StarlightCCVars.AutogammaRiftEnabled)) //#region Starlight Autogamma
         {
             var xform = Transform(rift);
             var station = _station.GetStationInMap(xform.MapID);
@@ -506,6 +508,7 @@ public sealed class RevSupplyRiftSystem : EntitySystem
                     Loc.GetString("centcomm-revs-gammarift"),
                     Loc.GetString("cmd-announce-sender"));
                 _alert.SetLevel(station.Value, "gamma", true, true, true, true);
+                _roundEnd.SetShuttleCallsEnabled(False);
             }
         } //#endregion Starlight Autogamma
         
