550442f80a2d24394f61f903f407c80164d763b8
AegisShieldDev <outrider.w@gmail.com>
2026-01-31 20:39:12 -0800

    Lets try 2.5x

--- Content.Server/_Starlight/StationEvents/Components/WreckSwarmComponent.cs
+++ Content.Server/_Starlight/StationEvents/Components/WreckSwarmComponent.cs
@@ -9,7 +9,7 @@ namespace Content.Server.StationEvents.Components;
 public sealed partial class WreckSwarmComponent : Component
 {
     [DataField]
-    public float Velocity = 60f;
+    public float Velocity = 50;
 
     /// <summary>
     /// The announcement played when a meteor swarm begins.
