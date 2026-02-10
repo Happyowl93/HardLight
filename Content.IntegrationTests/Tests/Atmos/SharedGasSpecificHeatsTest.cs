c9a7edc93bde571420422e4dc5d088a63c72bc44
CrazyPhantom779 <izayussedwards@gmail.com>
2026-02-11 09:27:42 +1100

    Revert that oop

--- Content.IntegrationTests/Tests/Atmos/SharedGasSpecificHeatsTest.cs
+++ Content.IntegrationTests/Tests/Atmos/SharedGasSpecificHeatsTest.cs
@@ -176,22 +176,13 @@ public sealed class SharedGasSpecificHeatsTest
         // ensure that replicated value changes by testing a new value
         const float newHeatScale = 13f;
 
-        // Starlight edit Start
-        // _sConfig = Server.ResolveDependency<IConfigurationManager>();
-        // _cConfig = Client.ResolveDependency<IConfigurationManager>();
-        // Starlight edit End
+        _sConfig = Server.ResolveDependency<IConfigurationManager>();
+        _cConfig = Client.ResolveDependency<IConfigurationManager>();
 
         await Server.WaitPost(delegate
         {
-            _sConfig = Server.ResolveDependency<IConfigurationManager>(); // Starlight
             _sConfig.SetCVar(CCVars.AtmosHeatScale, newHeatScale);
         });
-        // Starlight Start
-        await Client.WaitPost(delegate
-        {
-            _cConfig = Client.ResolveDependency<IConfigurationManager>();
-        });
-        // Starlight End
 
         await Server.WaitRunTicks(5);
         await Client.WaitRunTicks(5);
