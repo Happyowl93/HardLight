0350e5b501af35d28e2e07ce12f8397f24be60b8
CrazyPhantom779 <izayussedwards@gmail.com>
2026-02-15 12:24:04 +1100

    Lower maxLength

--- Content.Client/_Starlight/CustomObjectiveSummary/CustomObjectiveSummaryWindow.xaml.cs
+++ Content.Client/_Starlight/CustomObjectiveSummary/CustomObjectiveSummaryWindow.xaml.cs
@@ -15,7 +15,7 @@ public sealed partial class CustomObjectiveSummaryWindow : FancyWindow
 
     private SharedMindSystem? _mind;
 
-    private readonly int _maxLength = 256;
+    private readonly int _maxLength = 128;
 
     public event Action<string>? OnSubmitted;
 
