DeltaV-Station/Delta-v

Remove greentexts - Replace with custom responses (#2940)
* 1984

* my spelling....

* Admin log

* Fixes

* bruh

* Fixes thanks delta o7

* Pro fixes

Content.Client/_DV/CustomObjectiveSummary/CustomObjectiveSummaryUIController.cs
@@ -1,0 +1,44 @@
+using Content.Shared._DV.CustomObjectiveSummary;
+using Robust.Client.UserInterface.Controllers;
+using Robust.Shared.Network;
+
+namespace Content.Client._DV.CustomObjectiveSummary;
+
+public sealed class CustomObjectiveSummaryUIController : UIController
+{
+    [Dependency] private readonly IClientNetManager _net = default!;
+
+    private CustomObjectiveSummaryWindow? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeNetworkEvent<CustomObjectiveSummaryOpenMessage>(OnCustomObjectiveSummaryOpen);
+    }
+
+    private void OnCustomObjectiveSummaryOpen(CustomObjectiveSummaryOpenMessage msg, EntitySessionEventArgs args)
+    {
+        OpenWindow();
+    }
+
+    public void OpenWindow()
+    {
+        // If a window is already open, close it
+        _window?.Close();
+
+        _window = new CustomObjectiveSummaryWindow();
+        _window.OpenCentered();
+        _window.OnClose += () => _window = null;
+        _window.OnSubmitted += OnFeedbackSubmitted;
+    }
+
+    private void OnFeedbackSubmitted(string args)
+    {
+        var msg = new CustomObjectiveClientSetObjective
+        {
+            Summary = args,
+        };
+        _net.ClientSendMessage(msg);
+        _window?.Close();
+    }
+}

Content.Client/_DV/CustomObjectiveSummary/CustomObjectiveSummaryWindow.xaml.cs
@@ -1,0 +1,49 @@
+using Content.Client.UserInterface.Controls;
+using Content.Shared.Mind;
+using Robust.Client.AutoGenerated;
+using Robust.Client.Player;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Utility;
+
+namespace Content.Client._DV.CustomObjectiveSummary;
+
+[GenerateTypedNameReferences]
+public sealed partial class CustomObjectiveSummaryWindow : FancyWindow
+{
+    [Dependency] private readonly IPlayerManager _players = default!;
+    [Dependency] private readonly IEntityManager _entity = default!;
+
+    private SharedMindSystem? _mind;
+
+    private readonly int _maxLength = 256;
+
+    public event Action<string>? OnSubmitted;
+
+    public CustomObjectiveSummaryWindow()
+    {
+        RobustXamlLoader.Load(this);
+        IoCManager.InjectDependencies(this);
+
+        SubmitButton.OnPressed +=
+            _ => OnSubmitted?.Invoke(Rope.Collapse(ObjectiveSummaryTextEdit.TextRope));
+        ObjectiveSummaryTextEdit.OnTextChanged += _ => UpdateWordCount();
+
+        _mind ??= _entity.System<SharedMindSystem>();
+
+        _mind.TryGetMind(_players.LocalSession, out var mindUid, out _);
+
+        UpdateWordCount();
+
+        if (_entity.TryGetComponent<Shared._DV.CustomObjectiveSummary.CustomObjectiveSummaryComponent>(mindUid, out var summary))
+            ObjectiveSummaryTextEdit.TextRope = new Rope.Leaf(summary.ObjectiveSummary);
+
+        UpdateWordCount();
+    }
+
+    private void UpdateWordCount()
+    {
+        // Disable the button if its over the max length.sa
+        SubmitButton.Disabled = ObjectiveSummaryTextEdit.TextLength > _maxLength;
+        CharacterLimitLabel.Text = ObjectiveSummaryTextEdit.TextLength + "/" + _maxLength;
+    }
+}

Content.Shared.Database/LogType.cs
@@ -393,6 +393,7 @@ public enum LogType
     Tile = 86,
     BagOfHolding = 420, //Nyano - Summary: adds bag of holding.
     Psionics = 421, //Nyano - Summary: ads psionic as a log type.
+    ObjectiveSummary = 422, // DeltaV
 
     /// <summary>
     /// A client has sent too many chat messages recently and is temporarily blocked from sending more.

Content.Shared/_DV/CustomObjectiveSummary/CustomObjectiveSummaryComponent.cs
@@ -1,0 +1,16 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._DV.CustomObjectiveSummary;
+
+/// <summary>
+///     Put on a players mind if the wrote a custom summary for their objectives.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class CustomObjectiveSummaryComponent : Component
+{
+    /// <summary>
+    ///     What the player wrote as their summary!
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public string ObjectiveSummary = "";
+}

Happyowl93/HardLight

Content.Client/_Starlight/CustomObjectiveSummary/CustomObjectiveSummaryUIController.cs
@@ -1,0 +1,44 @@
+using Content.Shared._Starlight.CustomObjectiveSummary;
+using Robust.Client.UserInterface.Controllers;
+using Robust.Shared.Network;
+
+namespace Content.Client._Starlight.CustomObjectiveSummary;
+
+public sealed class CustomObjectiveSummaryUIController : UIController
+{
+    [Dependency] private readonly IClientNetManager _net = default!;
+
+    private CustomObjectiveSummaryWindow? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeNetworkEvent<CustomObjectiveSummaryOpenMessage>(OnCustomObjectiveSummaryOpen);
+    }
+
+    private void OnCustomObjectiveSummaryOpen(CustomObjectiveSummaryOpenMessage msg, EntitySessionEventArgs args)
+    {
+        OpenWindow();
+    }
+
+    public void OpenWindow()
+    {
+        // If a window is already open, close it
+        _window?.Close();
+
+        _window = new CustomObjectiveSummaryWindow();
+        _window.OpenCentered();
+        _window.OnClose += () => _window = null;
+        _window.OnSubmitted += OnFeedbackSubmitted;
+    }
+
+    private void OnFeedbackSubmitted(string args)
+    {
+        var msg = new CustomObjectiveClientSetObjective
+        {
+            Summary = args,
+        };
+        _net.ClientSendMessage(msg);
+        _window?.Close();
+    }
+}

Content.Client/_Starlight/CustomObjectiveSummary/CustomObjectiveSummaryWindow.xaml.cs
@@ -1,0 +1,49 @@
+using Content.Client.UserInterface.Controls;
+using Content.Shared.Mind;
+using Robust.Client.AutoGenerated;
+using Robust.Client.Player;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Utility;
+
+namespace Content.Client._Starlight.CustomObjectiveSummary;
+
+[GenerateTypedNameReferences]
+public sealed partial class CustomObjectiveSummaryWindow : FancyWindow
+{
+    [Dependency] private readonly IPlayerManager _players = default!;
+    [Dependency] private readonly IEntityManager _entity = default!;
+
+    private SharedMindSystem? _mind;
+
+    private readonly int _maxLength = 256;
+
+    public event Action<string>? OnSubmitted;
+
+    public CustomObjectiveSummaryWindow()
+    {
+        RobustXamlLoader.Load(this);
+        IoCManager.InjectDependencies(this);
+
+        SubmitButton.OnPressed +=
+            _ => OnSubmitted?.Invoke(Rope.Collapse(ObjectiveSummaryTextEdit.TextRope));
+        ObjectiveSummaryTextEdit.OnTextChanged += _ => UpdateWordCount();
+
+        _mind ??= _entity.System<SharedMindSystem>();
+
+        _mind.TryGetMind(_players.LocalSession, out var mindUid, out _);
+
+        UpdateWordCount();
+
+        if (_entity.TryGetComponent<Shared._Starlight.CustomObjectiveSummary.CustomObjectiveSummaryComponent>(mindUid, out var summary))
+            ObjectiveSummaryTextEdit.TextRope = new Rope.Leaf(summary.ObjectiveSummary);
+
+        UpdateWordCount();
+    }
+
+    private void UpdateWordCount()
+    {
+        // Disable the button if its over the max length.sa
+        SubmitButton.Disabled = ObjectiveSummaryTextEdit.TextLength > _maxLength;
+        CharacterLimitLabel.Text = ObjectiveSummaryTextEdit.TextLength + "/" + _maxLength;
+    }
+}

Content.Shared.Database/LogType.cs
@@ -391,6 +391,7 @@ public enum LogType
     /// Tiles related interactions.
     /// </summary>
     Tile = 86,
+    ObjectiveSummary = 422, // Starlight
 
     /// <summary>
     /// A client has sent too many chat messages recently and is temporarily blocked from sending more.

Content.Shared/_Starlight/CustomObjectiveSummary/CustomObjectiveSummaryComponent.cs
@@ -1,0 +1,16 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Starlight.CustomObjectiveSummary;
+
+/// <summary>
+///     Put on a players mind if the wrote a custom summary for their objectives.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class CustomObjectiveSummaryComponent : Component
+{
+    /// <summary>
+    ///     What the player wrote as their summary!
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public string ObjectiveSummary = "";
+}