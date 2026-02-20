b22f88fbfe80b6d6e93cf63d60cad960e6143058
walksanator <walkerffo22@gmail.com>
2026-02-19 21:32:26 -0500

    behold. a WORKING implementation.

--- Content.Shared/_Starlight/Movement/Components/RandomFootstepSoundComponent.cs
+++ Content.Shared/_Starlight/Movement/Components/RandomFootstepSoundComponent.cs
@@ -1,14 +1,15 @@
 ﻿using Content.Shared.FixedPoint;
 using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared._Starlight.Movement.Components;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class RandomFootstepSoundComponent : Component
 {
-    [DataField(required: true)]
+    [DataField(required: true), AutoNetworkedField]
     public SoundSpecifier? Sound;
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public float Chance = 0.001f; //1/1000 steps will make this sound.
 }
