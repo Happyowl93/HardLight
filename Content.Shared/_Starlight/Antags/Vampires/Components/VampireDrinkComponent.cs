6b7757b078e0a4e9ca10e4ce6725b1f6a1df607e
Lukeb28 <66802174+Lukeb28@users.noreply.github.com>
2026-02-26 15:17:55 -0500

    Create VampireDrinkComponent.cs

---
+++ Content.Shared/_Starlight/Antags/Vampires/Components/VampireDrinkComponent.cs
@@ -0,0 +1,24 @@
+namespace Content.Shared._Starlight.Antags.Vampires.Components;
+
+using Content.Shared._Starlight.Antags.Vampires.Prototypes;
+using Robust.Shared.Prototypes;
+
+/// <summary>
+/// Attach to a spawned action entity to define Vampire-specific drink related effects
+/// </summary>
+[RegisterComponent]
+public sealed partial class VampireDrinkComponent : Component
+{
+    [DataField]
+    public float HumanoidEfficiency = 0.5f;
+
+    [DataField]
+    public float NonHumanoidEfficiency = 0f; //Default to zero incase the line was removed from the yml
+
+    [DataField]
+    public DamageSpecifier DrinkDamage = new();
+
+    [DataField]
+    public float SipAmount = 10f;
+    
+}
