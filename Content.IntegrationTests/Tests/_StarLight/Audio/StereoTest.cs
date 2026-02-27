284e246b5e614d475b4bae53ced05a9a5e6e759d
Rinary <rinary.super@gmail.com>
2026-02-27 11:05:09 +0200

    ambience check

--- Content.IntegrationTests/Tests/_StarLight/Audio/StereoTest.cs
+++ Content.IntegrationTests/Tests/_StarLight/Audio/StereoTest.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using Content.Shared.Audio;
 using Robust.Client.Audio;
 using Robust.Client.ResourceManagement;
+using Robust.Shared.Audio;
 using Robust.Shared.ContentPack;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
 namespace Content.IntegrationTests.Tests._Starlight.Audio;
@@ -17,14 +20,35 @@ public sealed class StereoTest
         var client = pair.Client;
 
         var resMan = client.ResolveDependency<IResourceManager>();
+        var protoMan = client.ResolveDependency<IPrototypeManager>();
         var audioMan = client.ResolveDependency<IAudioManager>();
 
         var audioRoot = new ResPath("/Audio/");
 
         var badFiles = new Dictionary<string, string>();
 
+        var ambienceTracks = new List<ResPath>();
+        foreach (var ambience in protoMan.EnumeratePrototypes<AmbientMusicPrototype>())
+        {
+            switch (ambience.Sound)
+            {
+                case SoundCollectionSpecifier collection:
+                    if (collection.Collection == null)
+                        break;
+
+                    var slothCud = protoMan.Index<SoundCollectionPrototype>(collection.Collection);
+                    ambienceTracks.AddRange(slothCud.PickFiles);
+                    break;
+                case SoundPathSpecifier path:
+                    ambienceTracks.Add(path.Path);
+                    break;
+            }
+        }
+
         foreach (var file in resMan.ContentFindFiles(audioRoot))
         {
+            if (ambienceTracks.Contains(file))
+                continue; // Ambience tracks can be stereo, so we skip them.
             var ext = file.Extension.ToLowerInvariant();
             if (ext is not "ogg" and not "wav")
                 continue;
