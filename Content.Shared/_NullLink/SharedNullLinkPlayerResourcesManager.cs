e4eb4ca0b25432f264a7395d8ed22a1601315d39
Rinary <rinary.super@gmail.com>
2026-03-01 01:00:12 +0200

    patch 3

--- Content.Shared/_NullLink/SharedNullLinkPlayerResourcesManager.cs
+++ Content.Shared/_NullLink/SharedNullLinkPlayerResourcesManager.cs
@@ -32,7 +32,7 @@ public abstract class SharedNullLinkPlayerResourcesManager : ISharedNullLinkPlay
 
         var diff = value - data.Resources[id];
 
-        if (diff == 0) // If we don't have any difference - we don't need to call null link.
+        if (data.Resources.TryGetValue(id, our var oldValue) && value - oldValue == 0) // If we don't have any difference - we don't need to call null link.
             return false;
 
         data.Resources[id] = value;
