6b2b167767913fda2fed7015fa3b43d6d5a992bf
neomoth <69372252+neomoth@users.noreply.github.com>
2026-03-02 16:56:29 -0500

    turns out rinary was wrong, you do NOT need to do SHIT with ViewVariablesManager you just need the get;set; in the component

--- Content.Shared/_Starlight/Power/EntitySystems/BatterySelfRechargerSystem.cs
+++ Content.Shared/_Starlight/Power/EntitySystems/BatterySelfRechargerSystem.cs
@@ -7,16 +7,6 @@ namespace Content.Shared._Starlight.Power.EntitySystems;
 public sealed class BatterySelfRechargerSystem : EntitySystem
 {
     [Dependency] private readonly SharedBatterySystem _battery = default!;
-    [Dependency] private readonly IViewVariablesManager _vvm = default!;
-    
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        _vvm.GetTypeHandler<BatterySelfRechargerComponent>().AddPath(
-            nameof(BatterySelfRechargerComponent.AutoRechargeRateVV), (_, comp) => comp.AutoRechargeRateVV,
-            UpdateAutoRechargeRate);
-    }
     
     public void UpdateAutoRechargeRate(EntityUid uid, float value, BatterySelfRechargerComponent? comp)
     {
