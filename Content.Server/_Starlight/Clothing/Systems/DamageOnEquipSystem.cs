6652f8b69c8df9992566843d934b36cd4a00eadd
neomoth <69372252+neomoth@users.noreply.github.com>
2026-03-13 23:28:30 -0400

    less shitcode

--- Content.Server/_Starlight/Clothing/Systems/DamageOnEquipSystem.cs
+++ Content.Server/_Starlight/Clothing/Systems/DamageOnEquipSystem.cs
@@ -1,8 +1,9 @@
+using System.Linq;
 using Content.Server.Popups;
 using Content.Shared._Starlight.Clothing.Components;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Systems;
-using Content.Shared.GameTicking;
+using Content.Shared.Inventory;
 using Content.Shared.Inventory.Events;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Popups;
@@ -14,14 +15,11 @@ namespace Content.Server._Starlight.Clothing.Systems;
 public sealed class DamageOnEquipSystem : EntitySystem
 {
     [Dependency] private readonly DamageableSystem _damage = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly PopupSystem _popup = default!;
     [Dependency] private readonly ContainerSystem _container = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
     [Dependency] private readonly MobStateSystem _state = default!;
-
-    private readonly Dictionary<EntityUid, PendingDeath> _pendingDeaths = [];
-    private readonly Dictionary<EntityUid, PendingPopup> _pendingPopups = [];
-    private readonly List<EntityUid> _ignore = []; // ignore this time because they were forced to take it off by this system.
+    [Dependency] private readonly IGameTiming _timing = default!;
 
     public override void Initialize()
     {
@@ -29,68 +27,75 @@ public sealed class DamageOnEquipSystem : EntitySystem
 
         SubscribeLocalEvent<DamageOnEquipComponent, GotEquippedEvent>(OnGotEquipped);
         SubscribeLocalEvent<DamageOnEquipComponent, GotUnequippedEvent>(OnGotUnequipped);
-        SubscribeLocalEvent<RoundRestartCleanupEvent>(OnRoundRestartCleanup);
     }
 
     private void DoDamage(EntityUid uid, DamageOnEquipComponent comp, EntityUid target, DamageSpecifier damage)
     {
-        if(_pendingDeaths.ContainsKey(target)) return;
-        if (_ignore.Remove(target)) return;
+        if (comp.IgnoredEntities.Remove(target)) return;
+        if (comp.PendingEquipDamages.Any(pending => pending.Damager == uid && pending.Target == target)) return;
         if (!comp.CanDamageDead && _state.IsDead(target)) return;
+        if (!comp.CanDamageCrit && _state.IsCritical(target)) return;
         var popupDelay = comp.PopupLocId is not null ? comp.PopupDelay : TimeSpan.Zero;
-        var damageDelay = comp.PopupDelay + comp.Delay;
+        Log.Info($"popupDelay: {popupDelay}");
+        var damageDelay = comp.PopupDelay + comp.DamageDelay;
+        Log.Info($"damageDelay: {damageDelay}");
 
         if (popupDelay == TimeSpan.Zero && comp.PopupLocId is not null)
-            _popup.PopupEntity(Loc.GetString(comp.PopupLocId), uid, uid, PopupType.MediumCaution);
+            _popup.PopupEntity(Loc.GetString(comp.PopupLocId), target, target, PopupType.MediumCaution);
         if (damageDelay == TimeSpan.Zero)
         {
             _damage.ChangeDamage(target, damage, comp.IgnoreResistances, comp.InterruptDoAfters,
-                uid, comp.IgnoreGlobalModifiers, comp.ArmorPenetration, comp.CanHeal);
-            if (comp.DropOnKill && _state.IsDead(target))
+                target, comp.IgnoreGlobalModifiers, comp.ArmorPenetration, comp.CanHeal);
+            if ((comp.DropOnKill && _state.IsDead(target)) || (comp.DropOnCrit && _state.IsCritical(target)))
             {
-                _ignore.Add(target);
+                if(_inventory.InSlotWithFlags(uid, comp.TargetSlots)) comp.IgnoredEntities.Add(target);
                 _container.TryRemoveFromContainer(uid, comp.ForceDrop);
             }
             return;
         }
 
         if (comp.PopupLocId is { } locId && popupDelay > TimeSpan.Zero) // should be mutually exclusive with the check above
-            _pendingPopups.Add(target, new PendingPopup(uid, _timing.CurTime + popupDelay, locId));
-        _pendingDeaths.Add(target, new PendingDeath(uid, _timing.CurTime + damageDelay, damage));
+            comp.PendingEquipDamagePopups.Add(new PendingEquipDamagePopup(target, uid, _timing.CurTime + popupDelay, locId));
+        comp.PendingEquipDamages.Add(new PendingEquipDamage(target, uid, _timing.CurTime + damageDelay, damage));
     }
 
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
-        foreach (var (uid, pending) in _pendingPopups)
+        var query = EntityQueryEnumerator<DamageOnEquipComponent>();
+        while (query.MoveNext(out var uid, out var comp))
         {
-            if (Deleted(uid) || Deleted(pending.Damager))
+            foreach (var pending in comp.PendingEquipDamagePopups.ToList())
             {
-                _pendingPopups.Remove(uid);
-                continue;
+                if (Deleted(uid) || Deleted(pending.Target))
+                {
+                    comp.PendingEquipDamagePopups.Remove(pending);
+                    continue;
+                }
+                if (_timing.CurTime < pending.Delay) continue;
+                comp.PendingEquipDamagePopups.Remove(pending);
+                _popup.PopupEntity(Loc.GetString(pending.Popup), pending.Target, pending.Target, PopupType.MediumCaution);
             }
-            if (_timing.CurTime < pending.Delay) continue;
-            _pendingPopups.Remove(uid);
-            _popup.PopupEntity(Loc.GetString(pending.Popup), uid, uid, PopupType.MediumCaution);
-        }
-        
-        foreach (var (uid, pending) in _pendingDeaths)
-        {
-            if (Deleted(uid) || Deleted(pending.Damager))
+            
+            foreach (var pending in comp.PendingEquipDamages.ToList())
             {
-                _pendingPopups.Remove(uid);
-                continue;
-            }
-            if(_timing.CurTime < pending.Delay) continue;
-            _pendingDeaths.Remove(uid);
-            if (!TryComp<DamageOnEquipComponent>(pending.Damager, out var comp))
-                continue;
-            _damage.ChangeDamage(uid, pending.Damage, comp.IgnoreResistances, comp.InterruptDoAfters,
-                uid, comp.IgnoreGlobalModifiers, comp.ArmorPenetration, comp.CanHeal);
-            if (comp.DropOnKill && _state.IsDead(uid))
-            {
-                _ignore.Add(uid);
-                _container.TryRemoveFromContainer(pending.Damager, comp.ForceDrop);
+                if (Deleted(uid) || Deleted(pending.Target))
+                {
+                    comp.PendingEquipDamages.Remove(pending);
+                    continue;
+                }
+                if(_timing.CurTime < pending.Delay) continue;
+                comp.PendingEquipDamages.Remove(pending);
+                if (comp.IgnoredEntities.Remove(pending.Target)) continue;
+                if (!comp.CanDamageDead && _state.IsDead(pending.Target)) continue;
+                if (!comp.CanDamageCrit && _state.IsCritical(pending.Target)) continue;
+                _damage.ChangeDamage(pending.Target, pending.Damage, comp.IgnoreResistances, comp.InterruptDoAfters,
+                    pending.Target, comp.IgnoreGlobalModifiers, comp.ArmorPenetration, comp.CanHeal);
+                if ((comp.DropOnKill && _state.IsDead(pending.Target)) || (comp.DropOnCrit && _state.IsCritical(pending.Target)))
+                {
+                    if(_inventory.InSlotWithFlags(uid, comp.TargetSlots)) comp.IgnoredEntities.Add(pending.Target);
+                    _container.TryRemoveFromContainer(uid, comp.ForceDrop);
+                }
             }
         }
     }
@@ -108,14 +113,4 @@ public sealed class DamageOnEquipSystem : EntitySystem
         if (comp.UnequipDamage is null) return;
         DoDamage(uid, comp, ev.Equipee, comp.UnequipDamage);
     }
-
-    private void OnRoundRestartCleanup(RoundRestartCleanupEvent ev)
-    {
-        _pendingDeaths.Clear();
-        _pendingPopups.Clear();
-        _ignore.Clear();
-    }
-
-    private record struct PendingDeath(EntityUid Damager, TimeSpan Delay, DamageSpecifier Damage);
-    private record struct PendingPopup(EntityUid Damager, TimeSpan Delay, LocId Popup);
 }
