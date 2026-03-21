4d53cd2e37b4af8ba2d514f6b1e8f8e2257cdf32
Rinary <rinary.super@gmail.com>
2026-03-21 16:06:49 +0200

    quick fix

--- Content.Shared/Wieldable/SharedWieldableSystem.cs
+++ Content.Shared/Wieldable/SharedWieldableSystem.cs
@@ -195,7 +195,7 @@ public abstract class SharedWieldableSystem : EntitySystem
 
     private void OnUseInHand(EntityUid uid, WieldableComponent component, UseInHandEvent args)
     {
-        if (args.Handled || (TryComp<ChamberMagazineAmmoProviderComponent>(uid, out var chamber) && !chamber.BoltClosed && _hands.GetEmptyHandCount(args.User) == 0))
+        if (args.Handled || (TryComp<ChamberMagazineAmmoProviderComponent>(uid, out var chamber) && chamber.BoltClosed == false && _hands.GetEmptyHandCount(args.User) == 0))
             return;
 
         if (!component.Wielded)
