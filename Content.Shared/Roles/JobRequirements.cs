Happyowl93/HardLight

commit 5bef6dd1e62fd9bc715e3aa585692b7f13b171be

Always show job, antag, ghost role and loadout requirements (#3885)
<!-- IT'S NOT WIZDENS REPO, IF YOU WANT TO ADD YOUR CHANGES ON ALL
SERVERS, CREATE PR TO WIZDENS REPO -->

## Short description
<!-- What do you propose to change with your PR? -->

Always show job, antag, ghost role and loadout requirements, even if you
meet them.

Port of my https://github.com/space-wizards/space-station-14/pull/31157.

## Why we need to add this
<!-- What is the reason for adding these changes? Please post links to
Discussions as well as Bug Reports here. Please describe how this will
change the game balance. -->

It comes up sooooo much that people don't actually know what the role
requirements are for the roles they've already unlocked.

No more! Now everyone can see how they fare in comparison to the role
requirements. This includes staff! No longer will we have everything
unlocked _and have to wonder if we actually meet the requirements_.

## Media (Video/Screenshots)
<!--
If your PR contains in-game changes you must provide screenshots/videos
of the changes.
-->

### Character editor

<img width="1543" height="493" alt="Content Client_GeJgFqzPkt"
src="https://github.com/user-attachments/assets/8813f539-df6e-46e0-aa74-c20d587b811f"
/>

<img width="1663" height="251" alt="Content Client_skgWIicSO7"
src="https://github.com/user-attachments/assets/a4b4b3d9-8547-4053-9dd6-e16a444244a6"
/>

<img width="1491" height="291" alt="image"
src="https://github.com/user-attachments/assets/53a75848-7af9-43dd-992c-490dae93f980"
/>

<img width="661" height="213" alt="image"
src="https://github.com/user-attachments/assets/34e0ecd7-c716-4f5c-9809-3cdf0bfb7341"
/>


### Late join

<img width="1431" height="373" alt="image"
src="https://github.com/user-attachments/assets/608a1477-9c2d-4448-9ceb-b9bac14bf0d0"
/>

### Ghost roles

<img width="861" height="383" alt="image"
src="https://github.com/user-attachments/assets/532736c0-11ab-41ab-a7a6-049b1d41575b"
/>

Content.Shared/Roles/JobRequirements.cs
@@ -21,7 +21,7 @@ public static class JobRequirements
         JobPrototype job,
         ICommonSession? player,
         IReadOnlyDictionary<string, TimeSpan>? playTimes,
-        [NotNullWhen(false)] out FormattedMessage? reason,
+        out List<FormattedMessage> reason, // Starlight: List
         IEntityManager entManager,
         IPrototypeManager protoManager,
         HumanoidCharacterProfile? profile)
@@ -42,22 +42,26 @@ public static class JobRequirements
         HashSet<JobRequirement>? requirements,
         ICommonSession? player,
         IReadOnlyDictionary<string, TimeSpan>? playTimes,
-        [NotNullWhen(false)] out FormattedMessage? reason,
+        out List<FormattedMessage> reasons, // Starlight
         IEntityManager entManager,
         IPrototypeManager protoManager,
         HumanoidCharacterProfile? profile)
     {
-        reason = null;
+        reasons = new List<FormattedMessage>(); // Starlight
         if (requirements == null)
             return true;
 
+        var success = true; // Starlight
         foreach (var requirement in requirements)
         {
-            if (!requirement.Check(entManager, player, protoManager, profile, playTimes, out reason))
-                return false;
+            // Starlight BEGIN: Accumulate reason texts
+            if (!requirement.Check(entManager, player, protoManager, profile, playTimes, out var reason))
+                success = false;
+            reasons.Add(reason);
+            // Starlight END
         }
 
-        return true;
+        return success; // Starlight
     }
 
     public static bool TryRequirementsMet(
@@ -93,5 +97,5 @@ public abstract partial class JobRequirement
         IPrototypeManager protoManager,
         HumanoidCharacterProfile? profile,
         IReadOnlyDictionary<string, TimeSpan>? playTimes,
-        [NotNullWhen(false)] out FormattedMessage? reason);
+        out FormattedMessage reason); // Starlight: Always return reason
 }

Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -917,9 +917,8 @@ namespace Content.Client.Lobby.UI
                 selector.OnOpenGuidebook += OnOpenGuidebook;
 
                 var title = Loc.GetString(antag.Name);
-                var description = Loc.GetString(antag.Objective);
-                selector.Setup(items, title, 250, description, guides: antag.Guides);
-                selector.Select(Profile?.AntagPreferences.Contains(antag.ID) == true ? 0 : 1);
+                var description = FormattedMessage.FromMarkupPermissive(Loc.GetString(antag.Objective)); // Starlight
+                // Starlight: Setup & Select call moved down since we append requirements to the description.
 
                 if (!_requirements.IsAllowed(
                         antag,
@@ -937,6 +936,21 @@ namespace Content.Client.Lobby.UI
                 {
                     selector.UnlockRequirements();
                 }
+                
+                // Starlight BEGIN: Always show job requirements, even when they're met
+                // Append requirement details to description, separated by a clear line
+                if (!reason.IsEmpty)
+                {
+                    if (!description.IsEmpty)
+                    {
+                        description.PushNewline();
+                        description.PushNewline();
+                    }
+                    description.AddMessage(reason);
+                }
+                selector.Setup(items, title, 250, description, guides: antag.Guides);
+                selector.Select(Profile?.AntagPreferences.Contains(antag.ID) == true ? 0 : 1);
+                // Starlight END
 
                 selector.OnSelected += preference =>
                 {
@@ -1218,11 +1232,26 @@ namespace Content.Client.Lobby.UI
                     };
                     var jobIcon = _prototypeManager.Index(job.Icon);
                     icon.Texture = _sprite.Frame0(jobIcon.Icon);
-                    selector.Setup(items, job.LocalizedName, 200, job.LocalizedDescription, icon, job.Guides);
-
-                    if (!_requirements.IsAllowed(job, Profile, out var reason))
+                    // Starlight BEGIN: Always show job requirements
+                    var description = job.LocalizedDescription != null
+                        ? FormattedMessage.FromUnformatted(job.LocalizedDescription)
+                        : FormattedMessage.Empty;
+                    var allowed = _requirements.IsAllowed(job, Profile, out var reason);
+                    
+                    // Append the reason to the description.
+                    if (!description.IsEmpty)
                     {
-                        selector.LockRequirements(reason);
+                        description.PushNewline();
+                        description.PushNewline();
+                    }
+                    description.AddMessage(!reason.IsEmpty ? reason : FormattedMessage.FromMarkupPermissive(Loc.GetString("job-no-requirements")));
+                    
+                    selector.Setup(items, job.LocalizedName, 200, description, icon, job.Guides);
+
+                    if (!allowed)
+                    {
+                        selector.LockRequirements(description);
+                        // Starlight END
                         Profile = Profile?.WithoutJob(job);
                         SetDirty();
                     }

Content.Client/Lobby/UI/JobPriorityEditor.xaml.cs
@@ -171,7 +171,10 @@ public sealed partial class JobPriorityEditor : BoxContainer
                 };
                 var jobIcon = _prototypeManager.Index(job.Icon);
                 icon.Texture = jobIcon.Icon.Frame0();
-                selector.Setup(selectorPriorities, job.LocalizedName, 200, job.LocalizedDescription, icon, job.Guides);
+                var description = job.LocalizedDescription != null // Starlight BEGIN
+                    ? FormattedMessage.FromUnformatted(job.LocalizedDescription)
+                    : FormattedMessage.Empty;
+                selector.Setup(selectorPriorities, job.LocalizedName, 200, description, icon, job.Guides); // Starlight END
 
                 // This shouldn't depend on any character specific properties, so pass null
                 if (!_requirements.IsAllowed(job, null, out var reason))

Content.Client/Lobby/UI/LateJoinGui.xaml.cs
@@ -299,27 +299,15 @@ namespace Content.Client.Lobby.UI
                         // just send a -1 if there is no selected slot... catch it later
                         jobButton.OnPressed += _ => SelectedId.Invoke((id, _selectedSlot ?? -1, jobButton.JobId));
 
-                        if (!_jobRequirements.IsAllowed(prototype, humanoid, out var reason))
-                        {
-                            jobButton.Disabled = true;
+                        // Starlight BEGIN
+                        var allowed = _jobRequirements.IsAllowed(prototype, humanoid, out var reason);
+                        jobButton.Disabled = !allowed;
+                        
+                        var tooltip = new Tooltip();
+                        tooltip.SetMessage(!reason.IsEmpty ? reason : FormattedMessage.FromMarkupPermissive(Loc.GetString("job-no-requirements")));
+                        jobButton.TooltipSupplier = _ => tooltip;
 
-                            if (!reason.IsEmpty)
-                            {
-                                var tooltip = new Tooltip();
-                                tooltip.SetMessage(reason);
-                                jobButton.TooltipSupplier = _ => tooltip;
-                            }
-
-                            jobSelector.AddChild(new TextureRect
-                            {
-                                TextureScale = new Vector2(0.4f, 0.4f),
-                                Stretch = TextureRect.StretchMode.KeepCentered,
-                                Texture = _sprites.Frame0(new SpriteSpecifier.Texture(new ("/Textures/Interface/Nano/lock.svg.192dpi.png"))),
-                                HorizontalExpand = true,
-                                HorizontalAlignment = HAlignment.Right,
-                            });
-                        }
-                        else if (value == 0)
+                        if (allowed && value == 0) // Starlight END
                         {
                             jobButton.Disabled = true;
                         }

Content.Client/Lobby/UI/Loadouts/LoadoutContainer.xaml.cs
@@ -26,19 +26,18 @@ public sealed partial class LoadoutContainer : BoxContainer
         set => SelectButton.Text = value;
     }
 
-    public LoadoutContainer(ProtoId<LoadoutPrototype> proto, bool disabled, FormattedMessage? reason)
+    public LoadoutContainer(ProtoId<LoadoutPrototype> proto, bool disabled, FormattedMessage reason) // Starlight: Always has reason
     {
         RobustXamlLoader.Load(this);
         IoCManager.InjectDependencies(this);
 
         SelectButton.Disabled = disabled;
 
-        if (disabled && reason != null)
-        {
-            var tooltip = new Tooltip();
-            tooltip.SetMessage(reason);
-            SelectButton.TooltipSupplier = _ => tooltip;
-        }
+        // Starlight BEGIN
+        var tooltip = new Tooltip();
+        tooltip.SetMessage(!reason.IsEmpty ? reason : FormattedMessage.FromMarkupPermissive(Loc.GetString("loadout-no-requirements")));
+        SelectButton.TooltipSupplier = _ => tooltip;
+        // Starlight END
 
         if (_protoManager.Resolve(proto, out var loadProto))
         {

Content.Client/Lobby/UI/Roles/RequirementsSelector.xaml.cs
@@ -79,7 +79,7 @@ public sealed partial class RequirementsSelector : BoxContainer
         (string, int)[] items,
         string title,
         int titleSize,
-        string? description,
+        FormattedMessage description, // Starlight
         TextureRect? icon = null,
         List<ProtoId<GuideEntryPrototype>>? guides = null)
     {
@@ -93,7 +93,9 @@ public sealed partial class RequirementsSelector : BoxContainer
 
         TitleLabel.Text = title;
         TitleLabel.MinSize = new Vector2(titleSize, 0f);
-        TitleLabel.ToolTip = description;
+        var tooltip = new Tooltip(); // Starlight BEGIN
+        tooltip.SetMessage(description);
+        TitleLabel.TooltipSupplier = (_) => tooltip; // Starlight END
 
         if (icon != null)
         {

Content.Client/Players/PlayTimeTracking/JobRequirementsManager.cs
@@ -181,9 +181,9 @@ public sealed class JobRequirementsManager : ISharedPlaytimeManager
         List<ProtoId<JobPrototype>>? jobs,
         List<ProtoId<AntagPrototype>>? antags,
         HumanoidCharacterProfile? profile,
-        [NotNullWhen(false)] out FormattedMessage? reason)
+        out FormattedMessage reason) // Starlight: Always return reason
     {
-        reason = null;
+        reason = new FormattedMessage(); // Starlight
 
         if (antags is not null)
         {
@@ -212,12 +212,12 @@ public sealed class JobRequirementsManager : ISharedPlaytimeManager
     public bool IsAllowed(
         JobPrototype job,
         HumanoidCharacterProfile? profile,
-        [NotNullWhen(false)] out FormattedMessage? reason)
+        out FormattedMessage reason) // Starlight: Always return reason
     {
         // Check the player's bans
         if (_jobBans.Contains(job.ID))
         {
-            reason = FormattedMessage.FromUnformatted(Loc.GetString("role-ban"));
+            reason = FormattedMessage.FromMarkupPermissive(Loc.GetString("role-ban")); // Starlight: Formatted
             return false;
         }
 
@@ -243,7 +243,7 @@ public sealed class JobRequirementsManager : ISharedPlaytimeManager
     public bool IsAllowed(
         AntagPrototype antag,
         HumanoidCharacterProfile? profile,
-        [NotNullWhen(false)] out FormattedMessage? reason)
+        out FormattedMessage reason) // Starlight: Always return reason
     {
         // Check the player's bans
         if (_antagBans.Contains(antag.ID))
@@ -271,50 +271,52 @@ public sealed class JobRequirementsManager : ISharedPlaytimeManager
     /// <summary>
     /// SL: Check against a requirements list without a role. Avoid using if there's a role, as this doesn't check bans.
     /// </summary>
-    public bool CheckRequirementsForNonRole(HashSet<JobRequirement>? requirements, ICommonSession? player, HumanoidCharacterProfile? profile, [NotNullWhen(false)] out FormattedMessage? reason)
+    public bool CheckRequirementsForNonRole(HashSet<JobRequirement>? requirements, ICommonSession? player, HumanoidCharacterProfile? profile, out FormattedMessage reason) // Starlight: Always return reason
     {
         return CheckRoleRequirements(requirements, player, profile, out reason);
     }
 
     // This must be private so code paths can't accidentally skip requirement overrides. Call this through IsAllowed()
-    private bool CheckRoleRequirements(HashSet<JobRequirement>? requirements, ICommonSession? player, HumanoidCharacterProfile? profile, [NotNullWhen(false)] out FormattedMessage? reason)
+    private bool CheckRoleRequirements(HashSet<JobRequirement>? requirements, ICommonSession? player, HumanoidCharacterProfile? profile, out FormattedMessage reason) // Starlight: Always return reason
     {
-        reason = null;
+        reason = new FormattedMessage(); // Starlight
 
         if (requirements == null || !_cfg.GetCVar(CCVars.GameRoleTimers))
             return true;
 
-        var reasons = new List<string>();
+        var success = true; // Starlight
         foreach (var requirement in requirements)
         {
-            if (requirement.Check(_entManager, player, _prototypes, profile, _mergedRoles, out var jobReason))
-                continue;
-
-            reasons.Add(jobReason.ToMarkup());
+            if (!requirement.Check(_entManager, player, _prototypes, profile, _mergedRoles, out var checkDetails))
+                success = false; // Starlight
+            
+            if (!reason.IsEmpty) // Starlight BEGIN
+                reason.PushNewline();
+            reason.AddMessage(checkDetails); // Starlight END
         }
-
-        reason = reasons.Count == 0 ? null : FormattedMessage.FromMarkupOrThrow(string.Join('\n', reasons));
-        return reason == null;
+        
+        return success; // Starlight
     }
 
-    public bool CheckWhitelist(JobPrototype job, [NotNullWhen(false)] out FormattedMessage? reason)
+    public bool CheckWhitelist(JobPrototype job, out FormattedMessage reason) // Starlight: Always return reason
     {
-        reason = default;
+        reason = FormattedMessage.FromMarkupPermissive(Loc.GetString("role-whitelisted")); // Starlight: Markup
+
         if (!_cfg.GetCVar(CCVars.GameRoleWhitelist))
             return true;
 
         if (job.Whitelisted && !_jobWhitelists.Contains(job.ID))
         {
-            reason = FormattedMessage.FromUnformatted(Loc.GetString("role-not-whitelisted"));
+            reason = FormattedMessage.FromMarkupPermissive(Loc.GetString("role-not-whitelisted")); // Starlight: Markup
             return false;
         }
 
         return true;
     }
 
-    public bool CheckWhitelist(AntagPrototype antag, [NotNullWhen(false)] out FormattedMessage? reason)
+    public bool CheckWhitelist(AntagPrototype antag, out FormattedMessage reason) // Starlight: Always return reason
     {
-        reason = default;
+        reason = FormattedMessage.Empty; // Starlight
 
         // TODO: Implement antag whitelisting.
 

Content.Client/UserInterface/Systems/Ghost/Controls/Roles/GhostRoleButtonsBox.xaml.cs
@@ -26,17 +26,16 @@ namespace Content.Client.UserInterface.Systems.Ghost.Controls.Roles
                 var button = new GhostRoleEntryButtons(role);
                 button.RequestButton.OnPressed += _ => OnRoleSelected?.Invoke(role);
                 button.FollowButton.OnPressed += _ => OnRoleFollow?.Invoke(role);
+                
+                // Starlight: Always show role requirements
+                var tooltip = new Tooltip();
+                tooltip.SetMessage(reason is { IsEmpty: false } ? reason : FormattedMessage.FromMarkupPermissive(Loc.GetString("ghost-role-no-requirements")));
+                button.RequestButton.TooltipSupplier = _ => tooltip;
+                // Starlight END
 
                 if (!hasAccess)
                 {
                     button.RequestButton.Disabled = true;
-
-                    if (reason != null && !reason.IsEmpty)
-                    {
-                        var tooltip = new Tooltip();
-                        tooltip.SetMessage(reason);
-                        button.RequestButton.TooltipSupplier = _ => tooltip;
-                    }
 
                     button.RequestButton.AddChild(new TextureRect
                     {

commit 0627d568dedf37cb30e706ac3c2afc2072796a7e

Fix unintended recursion in JobRequirements that causes test failure (#3941)
<!-- IT'S NOT WIZDENS REPO, IF YOU WANT TO ADD YOUR CHANGES ON ALL
SERVERS, CREATE PR TO WIZDENS REPO -->

## Short description
<!-- What do you propose to change with your PR? -->

Fix unintended recursion in JobRequirements that causes test failure
introduced by
https://github.com/ss14Starlight/space-station-14/pull/3885.

## Why we need to add this
<!-- What is the reason for adding these changes? Please post links to
Discussions as well as Bug Reports here. Please describe how this will
change the game balance. -->

Test failure at least, but possibly also in actual use.

The three `TryRequirementsMet` overloads originally had a call graph of
`third overload -> first overload -> second overload` but my change to
the method signature of _only_ the first and second made `third
overload` recurse endlessly.

**Please wait for tests not to crash from the stack overflow before
merging.**

## Media (Video/Screenshots)
<!--
If your PR contains in-game changes you must provide screenshots/videos
of the changes.
-->

N/a

## Checks
<!-- check boxes for faster reviewing of your PR -->

- [x] I do not require assistance to complete the PR.
- [x] Before posting/requesting review of a PR, I have verified that the
changes work.
- [x] I have added screenshots/videos of the changes, or this PR does
not change in-game mechanics.
- [x] I affirm that my changes are licensed under the [MIT
License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
and grant permission for use in this repository under its conditions.

Content.Shared/Roles/JobRequirements.cs
@@ -68,7 +68,7 @@ public static class JobRequirements
         ProtoId<JobPrototype> job,
         ICommonSession? player,
         IReadOnlyDictionary<string, TimeSpan>? playTimes,
-        [NotNullWhen(false)] out FormattedMessage? reason,
+        out List<FormattedMessage> reason, // Starlight: List
         IEntityManager entManager,
         IPrototypeManager protoManager,
         HumanoidCharacterProfile? profile)
@@ -76,7 +76,7 @@ public static class JobRequirements
         if (protoManager.TryIndex(job, out var jobProto))
             return TryRequirementsMet(jobProto, player, playTimes, out reason, entManager, protoManager, profile);
 
-        reason = FormattedMessage.FromUnformatted("Failed to get job prototype");
+        reason = [FormattedMessage.FromUnformatted("Failed to get job prototype")]; // Starlight: List
         return false;
     }
 }