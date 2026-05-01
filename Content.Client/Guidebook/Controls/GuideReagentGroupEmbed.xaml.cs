Happyowl93/HardLight

commit 89e2e1ab7a676b05b2491b8a4744fa560965e798

Fix tests failing and taking 10 thousand years to run, for the third time (#3989)
<!-- IT'S NOT WIZDENS REPO, IF YOU WANT TO ADD YOUR CHANGES ON ALL
SERVERS, CREATE PR TO WIZDENS REPO -->

## Short description
<!-- What do you propose to change with your PR? -->
First https://github.com/ss14Starlight/space-station-14/pull/1504 and
second https://github.com/ss14Starlight/space-station-14/pull/1529 for
reference
Needless to say if I have to do this a fourth time I'm rebasing
Afterlight to Far Horizons, where the tests actually reliably pass on a
day to day basis
For the love of god, actually keep tests running this time, actually
enforce them passing before merging a PR, and actually keep the tests
running instead of timing out after 60 minutes on the Starlight branch
(see here
https://github.com/ss14Starlight/space-station-14/actions/runs/23678277294/job/68985647664),
like every other repo does, I do not feel like spending another 5 hours
trying to figure this out with the help of wizden maintainers trying to
figure out what the hell is going on here
The fact that this is a recurring issue is an embarassment, not just in
this repo, but in Afterlight where we actually pay to run our tests,
faster than GitHub, then have them time out anyways.

Also make sure you have grid_fill set to true on the live servers, which
you should, presumably.
This also removes the Starlight Start and Starlight end markers added in
#2328 because it's a Starlight file.
I don't really know why the code is spawning a new map per station and
then pre-loading two shuttles into it but this PR is about fixing test
run times specifically, not that, which would be more involved.

**Edit:** Also wait for tests to pass here before merging this PR, this
is the thing that stood out on my computer but my computer is not the
GitHub runner which is a lot slower, I'll fix whatever else comes up
from the CI run on this PR

## Why we need to add this

Content.Client/Guidebook/Controls/GuideReagentGroupEmbed.xaml.cs
@@ -8,6 +8,7 @@ using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 
 namespace Content.Client.Guidebook.Controls;
 
@@ -22,6 +23,9 @@ public sealed partial class GuideReagentGroupEmbed : BoxContainer, IDocumentTag
 
     private readonly ISawmill _sawmill;
 
+    // Starlight
+    private List<ReagentPrototype>? _reagentsToAdd;
+
     public GuideReagentGroupEmbed()
     {
         RobustXamlLoader.Load(this);
@@ -34,11 +38,7 @@ public sealed partial class GuideReagentGroupEmbed : BoxContainer, IDocumentTag
     {
         var prototypes = _prototype.EnumeratePrototypes<ReagentPrototype>()
             .Where(p => p.Group.Equals(group)).OrderBy(p => p.LocalizedName);
-        foreach (var reagent in prototypes)
-        {
-            var embed = new GuideReagentEmbed(reagent);
-            GroupContainer.AddChild(embed);
-        }
+        _reagentsToAdd = prototypes.ToList(); // Starlight
     }
 
     public bool TryParseTag(Dictionary<string, string> args, [NotNullWhen(true)] out Control? control)
@@ -52,13 +52,30 @@ public sealed partial class GuideReagentGroupEmbed : BoxContainer, IDocumentTag
 
         var prototypes = _prototype.EnumeratePrototypes<ReagentPrototype>()
             .Where(p => p.Group.Equals(group)).OrderBy(p => p.LocalizedName);
-        foreach (var reagent in prototypes)
-        {
-            var embed = new GuideReagentEmbed(reagent);
-            GroupContainer.AddChild(embed);
-        }
+        _reagentsToAdd = prototypes.ToList(); // Starlight
 
         control = this;
         return true;
+    }
+
+    // Starlight
+    protected override void FrameUpdate(FrameEventArgs args)
+    {
+        base.FrameUpdate(args);
+
+        if (_reagentsToAdd == null)
+            return;
+
+        // Stop after adding 5 reagents, wait for next frame update
+        // This prevents it from lagging as much, and helps to not trigger
+        // the style update limit in engine
+        var count = _reagentsToAdd.Count;
+        for (var i = count - 1; i >= Math.Max(0, count - 5); i--)
+        {
+            var reagent = _reagentsToAdd[i];
+            _reagentsToAdd.RemoveAt(i);
+            var embed = new GuideReagentEmbed(reagent);
+            GroupContainer.AddChild(embed);
+        }
     }
 }

.github/workflows/build-test-debug.yml
@@ -51,7 +51,6 @@ jobs:
       shell: pwsh
       run: |
         $env:DOTNET_gcServer=1
-        $env:DOTNET_GCHeapHardLimit=0x3C0000000
         dotnet test --no-build --configuration DebugOpt Content.IntegrationTests/Content.IntegrationTests.csproj --logger trx -- NUnit.ConsoleOut=0 NUnit.MapWarningTo=Failed
 
     - name: Test results

Content.Server/_Starlight/AlertArmory/AlertArmorySystem.cs
@@ -1,35 +1,29 @@
-using System.Diagnostics.CodeAnalysis;
-using Content.Server.AlertLevel;
 using Content.Server.Chat.Systems;
 using Content.Server.Pinpointer;
 using Content.Server.Shuttles.Components;
 using Content.Server.Shuttles.Events;
 using Content.Server.Shuttles.Systems;
-using Content.Server.Station.Components;
 using Content.Server.Station.Events;
 using Content.Server.Station.Systems;
 using Content.Shared.Shuttles.Components;
 using Robust.Server.GameObjects;
 using Robust.Shared.Map;
-using Robust.Shared.Map.Components;
 using Robust.Shared.EntitySerialization.Systems;
-using Robust.Shared.EntitySerialization;
 using Robust.Shared.Utility;
 using Content.Shared.Station.Components;
 using Robust.Shared.Physics.Components;
 using System.Numerics;
-using System.Linq;
 using Content.Server._Starlight.Station;
+using Content.Shared.CCVar;
 using Content.Shared.Popups;
-using Content.Shared.Tag;
-using Robust.Shared.Prototypes;
 using Content.Shared.Mobs.Components;
-using Robust.Shared.Player;
+using Robust.Shared.Configuration;
 
 namespace Content.Server.Starlight.AlertArmory;
 
 public sealed class AlertArmorySystem : EntitySystem
 {
+    [Dependency] private readonly IConfigurationManager _config = default!;
     [Dependency] private readonly MapSystem _map = default!;
     [Dependency] private readonly MetaDataSystem _meta = default!;
     [Dependency] private readonly MapLoaderSystem _loader = default!;
@@ -63,7 +57,9 @@ public sealed class AlertArmorySystem : EntitySystem
     ///</summary>
     private void InitializeAlertArmoryStation(EntityUid uid, AlertArmoryStationComponent comp, StationPostInitEvent ev)
     {
-        //Starlight start
+        if (!_config.GetCVar(CCVars.GridFill))
+            return;
+
         if (TryComp<StationDataComponent>(uid, out var station))
             foreach (var grid in station.Grids)
             {
@@ -72,8 +68,7 @@ public sealed class AlertArmorySystem : EntitySystem
                     return;
                 break; // can break, we already found the grid that created this station
             }
-        //Starlight end
-        
+
         var map = _map.CreateMap(out var mapId);
         _meta.SetEntityName(map, $"AlertArmories {uid}");
 

Content.Shared/Abilities/Goliath/GoliathTentacleSystem.cs
@@ -56,6 +56,9 @@ public sealed class GoliathTentacleSystem : DelayableEntitySystem
                 _turf.IsTileBlocked(tileRef, CollisionGroup.Impassable))
                 return;
 
+            if (TerminatingOrDeleted(pos.EntityId))
+                return;
+
             if (_net.IsServer)
                 Spawn(args.EntityId, pos);
             if ((Action<EntityCoordinates>?)action is not null && spawnPos.TryDequeue(out var newPos))

Resources/Locale/en-US/_Starlight/status-effects/misc.ftl
@@ -1,13 +1,0 @@
-﻿# Upstream effects
-status-effect-misc-forced-sleep = forced sleep
-status-effect-misc-ssd-sleep = forced sleep
-status-effect-misc-drowsiness = drowsiness
-status-effect-misc-hallucinations = hallucinations
-status-effect-misc-woozy = woozy
-status-effect-misc-drunk = drunk
-
-# Starlight effects
-status-effect-misc-temporary-blindness = temporary blindness
-status-effect-misc-muted = mute
-status-effect-misc-the-dark = the dark
-status-effect-misc-holding-breath = holding breath

Resources/Maps/_Starlight/Shuttles/CC-NT/Decimus_Shuttle_Magnus.yml
@@ -4038,9 +4038,6 @@ entities:
     - type: Transform
       pos: -0.7342167,-3.2392855
       parent: 1
-    - type: SpecialRespawn
-      stationMap:
-      - 1
 - proto: OperatingTable
   entities:
   - uid: 523

Resources/Prototypes/Entities/StatusEffects/misc.yml
@@ -47,7 +47,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectForcedSleeping
-  name: status-effect-misc-forced-sleep # Starlight: Localize
+  name: forced sleep # Starlight: Localize # Localize what you are in an entity prototype those are localized BY DEFAULT HELLO?
   components:
   # Starlight START
   - type: StatusEffect
@@ -64,7 +64,7 @@
 - type: entity
   parent: MobStatusEffectBase # Not even rejuvenate can cure SSD...
   id: StatusEffectSSDSleeping
-  name: status-effect-misc-ssd-sleep # Starlight: Localize
+  name: ssd sleep
   components:
   - type: ForcedSleepingStatusEffect
   - type: StunnedStatusEffect
@@ -74,7 +74,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectDrowsiness
-  name: status-effect-misc-drowsiness # Starlight: Localize
+  name: drowsiness
   components:
   - type: DrowsinessStatusEffect
   # Starlight START
@@ -89,7 +89,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectSeeingRainbow
-  name: status-effect-misc-hallucinations # Starlight: Localize
+  name: hallucinations
   components:
   - type: SeeingRainbowsStatusEffect
   # Starlight START
@@ -104,7 +104,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectWoozy
-  name: status-effect-misc-woozy # Starlight: Localize
+  name: woozy
   components:
   - type: DrunkStatusEffect
   # Starlight START
@@ -119,4 +119,4 @@
 - type: entity
   parent: [ StatusEffectWoozy, StatusEffectSlurred ]
   id: StatusEffectDrunk
-  name: status-effect-misc-drunk # Starlight: Localize
+  name: drunk

Resources/Prototypes/_Starlight/Entities/StatusEffects/misc.yml
@@ -3,7 +3,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectTemporaryBlindness
-  name: status-effect-misc-temporary-blindness
+  name: temporary blindness
   components:
   - type: StatusEffect
     whitelist:
@@ -18,7 +18,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectMuted
-  name: status-effect-misc-muted
+  name: mute
   components:
     - type: StatusEffect
       whitelist:
@@ -34,7 +34,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectTheDark
-  name: status-effect-misc-the-dark
+  name: the dark
   components:
   - type: StatusEffect
     whitelist:
@@ -61,7 +61,7 @@
 - type: entity
   parent: MobStatusEffectDebuff
   id: StatusEffectHeldBreath
-  name: status-effect-misc-holding-breath
+  name: holding breath
   components:
   - type: StatusEffect
     whitelist:

commit 12dfa00b4782650193aed83fca53d75fdff16657

Fix guidebook sorting of chemicals and drinks (#4315)
## Short description
Fixes sorting of chemicals / drinks / reagents in the guidebook to once
again be alphabetical.

## Why we need to add this
Fixes a regression in #3989 which caused guidebook reagents to be listed
in inverse order. Cause of this is that the `FrameUpdate` performance
optimization iterates through the reagent list in inverse order, meaning
the output became inverted of the input.

https://github.com/ss14Starlight/space-station-14/blob/b190c6e9e00e0d6bbdc2e193b901bcd3c933ce84/Content.Client/Guidebook/Controls/GuideReagentGroupEmbed.xaml.cs#L76-L82
The simplest solution which does not touch the optimization above is to
pre-sort in inverse order instead, which results in the correct final
order.

## Media (Video/Screenshots)
<img width="500" alt="image"
src="https://github.com/user-attachments/assets/6ab621a6-85a9-4ecd-a23d-f8cd2a1d2a87"
/>


## Checks
- [X] I do not require assistance to complete the PR.
- [X] Before posting/requesting review of a PR, I have verified that the
changes work.
- [X] I have added screenshots/videos of the changes, or this PR does
not change in-game mechanics.
- [X] I affirm that my changes are licensed under the [MIT
License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
and grant permission for use in this repository under its conditions.

**Changelog**
:cl: Xale
- fix: Fixed guidebook sorting of chemicals and drinks to once again be
in alphabetical order.

Content.Client/Guidebook/Controls/GuideReagentGroupEmbed.xaml.cs
@@ -40,7 +40,7 @@ public sealed partial class GuideReagentGroupEmbed : BoxContainer, IDocumentTag,
     public GuideReagentGroupEmbed(string group) : this()
     {
         var prototypes = _prototype.EnumeratePrototypes<ReagentPrototype>()
-            .Where(p => p.Group.Equals(group)).OrderBy(p => p.LocalizedName);
+            .Where(p => p.Group.Equals(group)).OrderByDescending(p => p.LocalizedName); // Starlight - Order reversed to support FrameUpdate
         _reagentsToAdd = prototypes.ToList(); // Starlight
     }
 
@@ -54,7 +54,7 @@ public sealed partial class GuideReagentGroupEmbed : BoxContainer, IDocumentTag,
         }
 
         var prototypes = _prototype.EnumeratePrototypes<ReagentPrototype>()
-            .Where(p => p.Group.Equals(group)).OrderBy(p => p.LocalizedName);
+            .Where(p => p.Group.Equals(group)).OrderByDescending(p => p.LocalizedName); // Starlight - Order reversed to support FrameUpdate
         _reagentsToAdd = prototypes.ToList(); // Starlight
 
         control = this;