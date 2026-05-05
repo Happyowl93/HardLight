3db51fa1703dd4d3c7cc9288b45d621184eaf22d
Shades <45297731+ShadesMars@users.noreply.github.com>
2026-05-05 05:43:36 +0200

    Brainwashing Trait (#1616)
    * Modular Brainwashing
    
    * Brainwashing trait

--- Content.Server/_HL/Brainwashing/BrainwasherSystem.cs
+++ Content.Server/_HL/Brainwashing/BrainwasherSystem.cs
@@ -10,6 +10,8 @@ using Content.Shared.DoAfter;
 using Content.Shared.Flash;
 using Content.Shared.Flash.Components;
 using Content.Shared.Mindshield.Components;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Silicons.Borgs.Components;
 using Content.Shared.StatusEffect;
 using Content.Shared.Verbs;
 using Robust.Server.Audio;
@@ -19,7 +21,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Server._HL.Brainwashing;
 
-public sealed class BrainwashVizorSystem : SharedBrainwashVizorSystem
+public sealed class BrainwasherSystem : SharedBrainwasherSystem
 {
     [Dependency] private readonly SharedBrainwashedSystem _sharedBrainwashedSystem = default!;
     [Dependency] private readonly DoAfterSystem _doAfterSystem = default!;
@@ -34,13 +36,14 @@ public sealed class BrainwashVizorSystem : SharedBrainwashVizorSystem
     [Dependency] private readonly PopupSystem _popupSystem = default!;
     public override void Initialize()
     {
-        SubscribeLocalEvent<BrainwashVizorComponent, GetVerbsEvent<Verb>>(ConfigureVerb);
-        SubscribeLocalEvent<BrainwashVizorComponent, ClothingGotEquippedEvent>(OnEquipped);
-        SubscribeLocalEvent<BrainwashVizorComponent, ClothingGotUnequippedEvent>(OnUnequipped);
-        SubscribeLocalEvent<BrainwashVizorComponent, EngagedEvent>(Engaged);
+        SubscribeLocalEvent<BrainwasherComponent, GetVerbsEvent<Verb>>(ConfigureVerb);
+        SubscribeLocalEvent<BrainwasherComponent, GetVerbsEvent<InnateVerb>>(BrainwashingVerb);
+        SubscribeLocalEvent<BrainwasherComponent, ClothingGotEquippedEvent>((uid, component, args) => StartBrainwashing(uid, args.Wearer, component));
+        SubscribeLocalEvent<BrainwasherComponent, ClothingGotUnequippedEvent>(OnUnequipped);
+        SubscribeLocalEvent<BrainwasherComponent, EngagedEvent>(Engaged);
     }
 
-    private void OnUnequipped(EntityUid uid, BrainwashVizorComponent component, ClothingGotUnequippedEvent args)
+    private void OnUnequipped(EntityUid uid, BrainwasherComponent component, ClothingGotUnequippedEvent args)
     {
         if (component.DoAfter == null)
             return;
@@ -49,7 +52,7 @@ public sealed class BrainwashVizorSystem : SharedBrainwashVizorSystem
         component.DoAfter = null;
     }
 
-    private void Engaged(EntityUid uid, BrainwashVizorComponent component, EngagedEvent args)
+    private void Engaged(EntityUid uid, BrainwasherComponent component, EngagedEvent args)
     {
         component.DoAfter = null; // Informs the component the doAfter doesn't exist anymore
 
@@ -57,78 +60,117 @@ public sealed class BrainwashVizorSystem : SharedBrainwashVizorSystem
             return;
 
         var user = _entityManager.GetEntity(args.Wearer);
-        TryComp<BrainwashedComponent>(uid, out var brainwashedComponent);
         TryGetNetEntity(user, out var userNetEntity);
-        if (userNetEntity == null || brainwashedComponent == null)
+        if (userNetEntity == null)
             return;
 
-        var userIsMindshielded = HasComp<MindShieldComponent>(user);
-        if (userIsMindshielded)
+        if (!component.BypassMindshield && HasComp<MindShieldComponent>(user))
         {
             _popupSystem.PopupCoordinates("Installation failed!", user.ToCoordinates());
             return;
         }
 
-        _audioSystem.PlayPvs(brainwashedComponent.EngageSound, uid, new AudioParams());
+        _audioSystem.PlayPvs(component.EngageSound, uid, new AudioParams());
         _statusEffectsSystem.TryAddStatusEffect<FlashedComponent>(user,
             _flashSystem.FlashedKey,
             TimeSpan.FromSeconds(5),
             true);
         _stun.TrySlowdown(user, TimeSpan.FromSeconds(5), true, 0, 0);
-        TryComp<BrainwashedComponent>(user, out var newBrainwashedComponent);
-        if (newBrainwashedComponent == null)
-            AddComp<BrainwashedComponent>(user);
-        _sharedBrainwashedSystem.SetCompulsions(user, brainwashedComponent.Compulsions);
+        EnsureComp<BrainwashedComponent>(user, out var newBrainwashedComponent);
+        _sharedBrainwashedSystem.SetCompulsions(user, newBrainwashedComponent, component.Compulsions);
         var brainwashedEvent = new BrainwashedEvent();
         RaiseLocalEvent(user, brainwashedEvent);
         RaiseNetworkEvent(brainwashedEvent, user);
     }
 
-    private void OnEquipped(EntityUid uid, BrainwashVizorComponent component, ClothingGotEquippedEvent args)
+    private void StartBrainwashing(EntityUid uid, EntityUid target, BrainwasherComponent component)
     {
-        if (!_consentSystem.HasConsent(args.Wearer, "MindControl"))
-            return;
-        TryComp<BrainwashedComponent>(uid, out var brainwashedComponent);
-        TryGetNetEntity(args.Wearer, out var netEntity);
-        TryComp<DoAfterComponent>(args.Wearer, out var doAfterComponent);
-        if (netEntity == null || component.DoAfter != null || doAfterComponent == null || brainwashedComponent == null)
+        if (!_consentSystem.HasConsent(target, "MindControl") || HasComp<BorgChassisComponent>(target))
             return;
 
+        TryGetNetEntity(target, out var netEntity);
+        if (netEntity == null || component.DoAfter != null)
+            return;
+
+        TryComp<DoAfterComponent>(target, out var doAfterComponent);
         var doAfterArgs = new DoAfterArgs(_entityManager,
-            args.Wearer,
-            TimeSpan.FromSeconds(3),
+            target,
+            component.ChardingDuration,
             new EngagedEvent(netEntity.Value),
             uid)
         {
             RequireCanInteract = false,
         };
+
+        if (HasComp<MobStateComponent>(uid))
+        {
+            doAfterComponent = null;
+            if (TryComp<DoAfterComponent>(uid, out var newdoAfterComponent))
+                doAfterComponent = newdoAfterComponent;
+
+            doAfterArgs = new DoAfterArgs(_entityManager,
+            uid,
+            component.ChardingDuration,
+            new EngagedEvent(netEntity.Value),
+            uid)
+            {
+                RequireCanInteract = false,
+                BreakOnMove = true,
+                BreakOnDamage = true,
+            };
+        }
+
+        if (doAfterComponent == null)
+            return;
+
         var startDoAfterSuccess = _doAfterSystem.TryStartDoAfter(doAfterArgs, out var doAfterId, doAfterComponent);
         if (!startDoAfterSuccess)
             return;
+
         component.DoAfter = doAfterId;
-        _audioSystem.PlayPvs(brainwashedComponent.ChargingSound, uid, new AudioParams());
+        _audioSystem.PlayPvs(component.ChargingSound, uid, new AudioParams());
     }
 
-    private void ConfigureVerb(EntityUid uid, BrainwashVizorComponent component, GetVerbsEvent<Verb> args)
+    private void ConfigureVerb(EntityUid uid, BrainwasherComponent component, GetVerbsEvent<Verb> args)
     {
-        if (!args.CanAccess || !args.CanInteract)
+        if (!args.CanAccess || !args.CanInteract
+            || HasComp<MobStateComponent>(uid) && args.User != uid)
             return;
+
         args.Verbs.Add(new Verb
         {
             Act = () =>
             {
                 var ui = new BrainwashEditor(_sharedBrainwashedSystem);
-                TryComp<BrainwashedComponent>(uid, out var brainwashedComponent);
-                if (brainwashedComponent == null)
-                    return;
                 if (!_playerManager.TryGetSessionByEntity(args.User, out var session))
                     return;
                 _euiManager.OpenEui(ui, session);
-                ui.UpdateCompulsions(brainwashedComponent, uid);
+                ui.UpdateCompulsions(component, uid);
             },
-            Text = "Configure Neuralyzer",
+            Text = component.ConfigureText,
             Icon = new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/VerbIcons/sentient.svg.192dpi.png")),
             Priority = 1
         });
+    }
+
+    private void BrainwashingVerb(EntityUid uid, BrainwasherComponent component, GetVerbsEvent<InnateVerb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract || args.User != uid
+            || HasComp<BorgChassisComponent>(args.Target))
+            return;
+
+        if (uid != args.Target && _consentSystem.HasConsent(args.Target, "MindControl"))
+        {
+            args.Verbs.Add(new InnateVerb
+            {
+                Act = () =>
+                {
+                    StartBrainwashing(args.User, args.Target, component);
+                },
+                Text = "Brainwash",
+                Icon = new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/VerbIcons/sentient.svg.192dpi.png")),
+                Priority = 1
+            });
+        }
     }
 }
